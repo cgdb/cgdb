66d7f6dc7c3f51db0b9660c88103306ae133fdbc
Bob Rossi <bob@brasko.net>
2003-01-28 16:34:24 +0000

    configure
    git-svn-id: https://cgdb.svn.sourceforge.net/svnroot/cgdb/cgdb/trunk@19 61d65b3c-fd13-0410-b770-9cb5f9339520

--- autoregen.sh
+++ autoregen.sh
@@ -5,25 +5,25 @@ autoconf
 autoheader
 automake -a
 
-touch Makefile.in
-touch src/Makefile.in
-touch tgdb/Makefile.in
-touch tgdb/src/Makefile.in
+#touch Makefile.in
+#touch src/Makefile.in
+#touch tgdb/Makefile.in
+#touch tgdb/src/Makefile.in
 
-touch config.status
-touch stamp-h.in
-touch config-h.in
-touch configure
-touch aclocal.m4
+#touch config.status
+#touch stamp-h.in
+#touch config-h.in
+#touch configure
+#touch aclocal.m4
 
-touch Makefile.am
-touch src/Makefile.am
-touch tgdb/Makefile.am
-touch tgdb/src/Makefile.am
+#touch Makefile.am
+#touch src/Makefile.am
+#touch tgdb/Makefile.am
+#touch tgdb/src/Makefile.am
 
-touch configure.in
+#touch configure.in
 
-cvs commit -f Makefile.in src/Makefile.in tgdb/Makefile.in tgdb/src/Makefile.in \
-              config.status stamp-h.in config-h.in configure aclocal.m4 \
-              Makefile.am src/Makefile.am tgdb/Makefile.am tgdb/src/Makefile.am \
-              configure.in
+#cvs commit -f Makefile.in src/Makefile.in tgdb/Makefile.in tgdb/src/Makefile.in \
+#              config.status stamp-h.in config-h.in configure aclocal.m4 \
+#              Makefile.am src/Makefile.am tgdb/Makefile.am tgdb/src/Makefile.am \
+#              configure.in
