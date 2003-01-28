03f1216a0f8b2f37ad8290ad8356ecbd79700274
Bob Rossi <bob@brasko.net>
2003-01-28 16:06:51 +0000

    configure
    git-svn-id: https://cgdb.svn.sourceforge.net/svnroot/cgdb/cgdb/trunk@16 61d65b3c-fd13-0410-b770-9cb5f9339520

--- autoregen.sh
+++ autoregen.sh
@@ -1,5 +1,29 @@
 rm -rf autom4te.cache/
+
 aclocal
 autoconf
 autoheader
 automake -a
+
+touch Makefile.in
+touch src/Makefile.in
+touch tgdb/Makefile.in
+touch tgdb/src/Makefile.in
+
+touch config.status
+touch stamp-h.in
+touch config-h.in
+touch configure
+touch aclocal.m4
+
+touch Makefile.am
+touch src/Makefile.am
+touch tgdb/Makefile.am
+touch tgdb/src/Makefile.am
+
+touch configure.in
+
+cvs commit -f Makefile.in src/Makefile.in tgdb/Makefile.in tgdb/src/Makefile.in \
+              config.status stamp-h.in config-h.in configure aclocal.m4 \
+              Makefile.am src/Makefile.am tgdb/Makefile.am tgdb/src/Makefile.am \
+              configure.in
