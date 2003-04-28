daf7ebc4f50e7de7db86290147b9165504547d9d
Bob Rossi <bob@brasko.net>
2003-04-28 00:25:14 +0000

    Finish
    git-svn-id: https://cgdb.svn.sourceforge.net/svnroot/cgdb/cgdb/trunk@177 61d65b3c-fd13-0410-b770-9cb5f9339520

---
+++ tgdb/tgdb-base/src/test.c
@@ -0,0 +1,74 @@
+#include <stdio.h>
+#include <unistd.h>
+
+int two(int a){
+   return a*a;
+}
+
+int one(int a, int b){
+   return two(a) + two(b);
+}
+
+void short_func(void){
+   int i = 0;
+   sleep(1);
+   i = 1;
+   i = 2;
+}
+
+void long_func(void){
+   int k = 0;
+   sleep(3);
+   k = 1;
+   k = 2;
+}
+
+#define SIZE 100000
+
+struct test {
+   int a;
+   int b;
+   int c;  
+} a = { 1, 2, 3 };
+
+int main(int argc, char **argv){
+   int i = 3;
+   int j = 4;
+   char temp[SIZE];
+
+   long_func();
+
+   fprintf(stderr, "HAHAAHA\n");
+   fprintf(stderr, "AHAAHA\n");
+   fprintf(stderr, "HAAHA\n");
+   fprintf(stderr, "AAHA\n");
+   fprintf(stderr, "AHA\n");
+   fprintf(stderr, "HA\n");
+   fprintf(stderr, "A\n");
+
+   for ( i = 0; i < 100; i++)
+       fprintf(stderr, "Yo\n");
+
+   /*long_func();*/
+
+   short_func();
+   short_func();
+   short_func();
+
+   for(i = 0; i < 100000; ++i)
+      j++;
+
+   i = one(i, j);
+   j = two(i);
+
+   for(i = 0; i < 6; ++i)
+      fprintf(stderr, "returned from init\n");
+
+   for(i = 0; i < SIZE; ++i)
+      temp[i] = (i % 40) + 40;
+
+   i += j;
+   
+   return 0;
+}
+
---
+++ tgdb/tgdb-base/src/tgdb.c
@@ -0,0 +1,36 @@
+#include "tgdb.h"
+#include "a2-tgdb.h"
+#include "gdbmi_tgdb.h"
+
+int tgdb_init(void) {
+    if ( 1 ) {
+        tgdb_start                          = a2_tgdb_init;
+        tgdb_send                           = a2_tgdb_send;
+        tgdb_send_input                     = a2_tgdb_send_input;
+        tgdb_recv_input                     = a2_tgdb_recv_input;
+        tgdb_tty_send                       = a2_tgdb_tty_send;
+        tgdb_recv                           = a2_tgdb_recv;
+        tgdb_tty_recv                       = a2_tgdb_tty_recv;
+        tgdb_new_tty                        = a2_tgdb_new_tty;
+        tgdb_tty_name                       = a2_tgdb_tty_name;
+        tgdb_get_sources                    = a2_tgdb_get_sources;
+        tgdb_get_source_absolute_filename   = a2_tgdb_get_source_absolute_filename;
+        tgdb_err_msg                        = a2_tgdb_err_msg;
+        tgdb_shutdown                       = a2_tgdb_shutdown;
+    } else {
+        tgdb_start                          = gdbmi_tgdb_init;
+        tgdb_send                           = gdbmi_tgdb_send;
+        tgdb_send_input                     = gdbmi_tgdb_send_input;
+        tgdb_recv_input                     = gdbmi_tgdb_recv_input;
+        tgdb_tty_send                       = gdbmi_tgdb_tty_send;
+        tgdb_recv                           = gdbmi_tgdb_recv;
+        tgdb_tty_recv                       = gdbmi_tgdb_tty_recv;
+        tgdb_new_tty                        = gdbmi_tgdb_new_tty;
+        tgdb_tty_name                       = gdbmi_tgdb_tty_name;
+        tgdb_get_sources                    = gdbmi_tgdb_get_sources;
+        tgdb_get_source_absolute_filename   = gdbmi_tgdb_get_source_absolute_filename;
+        tgdb_err_msg                        = gdbmi_tgdb_err_msg;
+        tgdb_shutdown                       = gdbmi_tgdb_shutdown;
+    }
+    return 0;
+}
