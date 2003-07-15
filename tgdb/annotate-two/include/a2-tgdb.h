0720e11f7f279850d4aa128a1353ba1198bca4f5
Bob Rossi <bob@brasko.net>
2003-07-15 22:20:04 +0000

    Getting a good feeling for the .4 interface
    git-svn-id: https://cgdb.svn.sourceforge.net/svnroot/cgdb/cgdb/trunk@244 61d65b3c-fd13-0410-b770-9cb5f9339520

--- tgdb/annotate-two/include/a2-tgdb.h
+++ tgdb/annotate-two/include/a2-tgdb.h
@@ -12,6 +12,261 @@
 #include "types.h"
 #include "tgdb_interface.h"
 
+#if 0
+	/* Here is a prototype of the interface planned for .4.0 
+	 *
+	 */
+
+	/* annotate-two
+	 * ------------
+	 *
+	 * This struct is a reference to a libannotate-two instance.
+	 */
+	 struct annotate_two;
+
+    /* a2_create_instance
+	 * ------------------
+	 *
+	 * This invokes a libannotate_two library instance.
+	 *
+	 * The client must call this function before any other function in the 
+	 * tgdb library.
+	 *
+	 * debugger
+	 * --------
+     *  The path to the desired debugger to use. If this is NULL, then just
+	 *  "gdb" is used.
+	 *
+	 * argc
+	 * ----
+	 *  The number of arguments to pass to the debugger
+	 *
+	 * argv
+	 * ----
+	 *  The arguments to pass to the debugger    
+     *
+     * debugger_stdin
+     * --------------
+     *  Writing writes to debugger's stdin
+     *
+     * debugger_fd
+     * -----------
+	 *  The descriptor to the debugger's I/O
+     *
+     * config_dir
+     * ----------
+     *  The current config directory. Files can be stored here.
+     *
+     *  Returns
+     *  -------
+     *      NULL on error, A valid descriptor upon success
+     *
+	 */
+	struct annotate_two* tgdb_create_instance ( 
+		const char *debugger, 
+		int argc, char **argv,
+		int *debugger_stdin, int *debugger_fd, const char *config_dir );
+
+	
+	/* a2_tgdb_initialize
+	 * ------------------
+	 *
+	 *  This initializes the libannotate_two libarary.
+	 *  
+	 *  a2
+	 *  --
+	 *   The annotate two context.
+	 *
+	 *  inferior_tty_name
+	 *  -----------------
+	 *   The name of the tty to use for the inferior.
+	 */
+	int a2_tgdb_initialize ( struct annotate_two *a2, const char *inferior_tty_name );
+
+	/* a2_tgdb_shutdown
+	 * ----------------
+	 *
+	 *  Shuts down the annotate two context. No more calls can be made on the
+	 *  current context.
+	 *
+	 *  a2
+	 *  --
+	 *   The annotate two context.
+	 */
+	int a2_tgdb_shutdown ( struct annotate_two *a2 );
+
+	/* a2_tgdb_get_source_absolute_filename
+	 * ------------------------------------
+	 *
+	 *  Gets the Absolute path of FILE.
+	 *  
+	 *  a2
+	 *  --
+	 *   The annotate two context.
+	 *
+	 *  file
+	 *  ----
+	 *   The relative path that gdb outputted.
+	 */
+	int a2_tgdb_get_source_absolute_filename ( struct annotate_two *a2, const char *file );
+
+	/* a2_tgdb_get_sources
+	 * -------------------
+	 *
+	 *  Gets all the source files that the inferior makes up.
+	 *
+	 *  a2
+	 *  --
+	 *   The annotate two context.
+	 */
+	int a2_tgdb_get_sources ( struct annotate_two *a2 );
+
+	/* a2_set_inferior_tty
+	 * -------------------
+	 *
+	 *  Sets the inferior's new tty name.
+	 *
+	 *  a2
+	 *  --
+	 *   The annotate two context.
+	 *
+ 	 *  inferior_tty_name
+	 *  -----------------
+	 *   The name of the tty that the inferior should use.
+	 */
+	int a2_set_inferior_tty ( struct annotate_two *a2, const char *inferior_tty_name );
+
+	/* a2_tgdb_err_msg
+	 * ---------------
+	 *
+	 * Returns the last error message ?
+	 * Not implemented yet.
+	 *
+	 * a2
+	 * --
+	 *  The annotate two context.
+	 */
+	int a2_tgdb_err_msg ( struct annotate_two *a2 );
+
+	/* a2_tgdb_change_prompt
+	 * ---------------------
+	 *
+	 *  This will change the prompt the user sees to PROMPT.
+	 *
+	 * a2
+	 * --
+	 *  The annotate two context.
+	 *
+	 * prompt
+	 * ------
+	 *  The new prompt to change too.
+	 */
+	int a2_tgdb_change_prompt(struct annotate_two *a2, const char *prompt);
+
+	/* a2_tgdb_command_callback
+	 * ------------------------
+	 *
+ 	 * This is called when readline determines a command has been typed. 
+	 *
+	 * a2
+	 * --
+	 *  The annotate two context.
+	 *
+ 	 * command
+	 * ---- 
+	 *  The command the user typed without the '\n'.
+	 */
+	int a2_tgdb_command_callback(struct annotate_two *a2, const char *command);
+
+	/* a2_tgdb_completion_callback
+	 * ---------------------------
+	 *
+ 	 * This is called when readline determines a command needs to be completed.
+	 *
+	 * a2
+	 * --
+	 *  The annotate two context.
+	 *
+ 	 * command
+	 * ----
+	 *  The command to be completed
+	 */
+	int a2_tgdb_completion_callback(struct annotate_two *a2, const char *command);
+
+	/* a2_is_ready
+	 * -----------
+	 *
+	 * Returns 1 if ready to recieve data, otherwise 0
+	 *
+	 * a2
+	 * --
+	 *  The annotate two context.
+	 */
+	int a2_is_ready ( struct annotate_two *a2 );
+
+	/* a2_commands_typed_at_prompt
+	 * ---------------------------
+	 *
+ 	 * This functions recieves the characters the user has typed.
+ 	 * It only gets called if it is not busy ( the function above )
+	 *
+	 * This functions is a hack, and must be removed.
+	 *
+	 * a2
+	 * --
+	 *  The annotate two context.
+	 *
+	 * i
+	 * -
+	 *  ???
+	 */
+	void a2_command_typed_at_prompt ( struct annotate_two *a2, int i );
+
+	/* a2_tgdb_is_debugger_ready
+	 * -------------------------
+	 *
+ 	 * This determines if the annotate two subsystem is ready to have gdb recieve
+ 	 * another command.
+	 *
+	 * a2
+	 * --
+	 *  The annotate two context.
+	 *
+     * Returns
+	 * -------
+	 *  1 if it is ready, 0 if it is not.
+	 */
+	int a2_tgdb_is_debugger_ready(struct annotate_two *a2);
+
+	/* a2_tgdb_return_client_command
+	 * -----------------------------
+	 *
+ 	 *  This returns the command to send to gdb for the enum C.
+ 	 *  It will return NULL on error, otherwise correct string on output.
+	 *
+	 * a2
+	 * --
+	 *  The annotate two context.
+	 *
+	 * c
+	 * -
+	 *  The command to run.
+	 */
+	char *a2_tgdb_return_client_command ( struct annotate_two *a2, enum tgdb_command c );
+
+	/* a2_tgdb_client_modify_breakpoint
+	 * --------------------------------
+	 *
+	 * a2
+	 * --
+	 *  The annotate two context.
+	 *
+ 	 * Look at tgdb.h
+	 *
+	 */
+	char *a2_tgdb_client_modify_breakpoint ( struct annotate_two *a2, const char *file, int line, enum tgdb_breakpoint_action b );
+
+#endif
 
 /*  The only commands used
     ----------------------------
