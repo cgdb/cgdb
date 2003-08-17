606fd598ed62f60b266adabeabc28270b6cf633b
Bob Rossi <bob@brasko.net>
2003-08-17 13:58:20 +0000

    Adding doxygen config files, changed version number to -cvs
    git-svn-id: https://cgdb.svn.sourceforge.net/svnroot/cgdb/cgdb/trunk@272 61d65b3c-fd13-0410-b770-9cb5f9339520

--- tgdb/annotate-two/include/a2-tgdb.h
+++ tgdb/annotate-two/include/a2-tgdb.h
@@ -12,28 +12,30 @@
 #include "types.h"
 #include "tgdb_interface.h"
 
-/* Here is a prototype of the interface planned for .4.0 
- *
+/*! \file
+ * 		a2-tgdb.h
+ * 	\brief
+ * 		This interface documents the annotate two context.
  */
 
-/* annotate-two
- * ------------
+/** 
+ * annotate-two
  *
  * This struct is a reference to a libannotate-two instance.
  */
 struct annotate_two;
 
-/*  The only commands used
-    ----------------------------
-    info breakpoints    -> Get a list of breakpoints
-    tty                 -> Tell gdb where to send inferior's output
-    complete            -> Complete the current console line
-    info sources        -> Show all the sources inferior is made of
-    info source         -> Shows information on the current source file
-    list                -> displays the contents of current source file
-    complete arg        -> completes the argument arg
-*/
-
+/**  
+ * Commands
+ *
+ *   info breakpoints    -> Get a list of breakpoints
+ *   tty                 -> Tell gdb where to send inferior's output
+ *   complete            -> Complete the current console line
+ *   info sources        -> Show all the sources inferior is made of
+ *   info source         -> Shows information on the current source file
+ *   list                -> displays the contents of current source file
+ *   complete arg        -> completes the argument arg
+ */
 enum annotate_commands {
     ANNOTATE_VOID = 0,
     ANNOTATE_INFO_BREAKPOINTS,
@@ -47,72 +49,69 @@ enum annotate_commands {
     ANNOTATE_SET_PROMPT
 };
 
-/***************************************************************************
- * Starting and Stopping Commands.
- **************************************************************************/
+/******************************************************************************/
+/**
+ * @name Starting and Stopping Commands.
+ * These functions are for starting and stopping the annotate_two context.
+ */
+/******************************************************************************/
 
-/* a2_create_instance
- * ------------------
+//@{
+
+/** 
+ * a2_create_instance
  *
  * This invokes a libannotate_two library instance.
  *
  * The client must call this function before any other function in the 
  * tgdb library.
  *
- * debugger
- * --------
- *  The path to the desired debugger to use. If this is NULL, then just
+ * \param debugger
+ *	The path to the desired debugger to use. If this is NULL, then just
  *  "gdb" is used.
  *
- * argc
- * ----
+ * \param argc
  *  The number of arguments to pass to the debugger
  *
- * argv
- * ----
+ * \param argv
  *  The arguments to pass to the debugger    
  *
- * config_dir
- * ----------
+ * \param config_dir
  *  The current config directory. Files can be stored here.
  *
- * Returns
- * -------
+ * @return
  *  NULL on error, A valid descriptor upon success
- *
  */
 struct annotate_two* a2_create_instance ( 
 	const char *debugger, 
 	int argc, char **argv,
 	const char *config_dir );
 
-/* a2_initialize
- * -------------
+/** 
+ * a2_initialize
  *
  * This initializes the libannotate_two libarary.
  *  
- * a2
- * --
+ * \param a2
  *  The annotate two context.
  *
- * debugger_stdin
- * --------------
+ * \param debugger_stdin
  *  Writing to this descriptor, writes to the stdin of the debugger.
  *
- * debugger_stdout
- * ---------------
+ * \param debugger_stdout
  *  Reading from this descriptor, reads from the debugger's stdout.
  *
- * inferior_stdin
- * --------------
+ * \param inferior_stdin
  *  Writing to this descriptor, writes to the stdin of the inferior.
  *
- * inferior_stdout
- * ---------------
+ * \param inferior_stdout
  *  Reading from this descriptor, reads from the inferior's stdout.
  *
- * Retruns
- * -------
+ * \param command_finished
+ *  This function is called by the A2 context, whenever it finishes running
+ *  a command on behalf of its caller.
+ *
+ * @retrun Retruns
  *  0 on success, otherwise -1 on error.
  */
 int a2_initialize ( 
@@ -121,153 +120,151 @@ int a2_initialize (
 	int *inferior_stdin, int *inferior_stdout,
 	command_completed command_finished);
 
-/* a2_shutdown
- * -----------
+/** a2_shutdown
  *
  * Shuts down the annotate two context. No more calls can be made on the
  * current context. It will clean up after itself. All descriptors it 
  * opened, it will close.
  *
- * a2
- * --
+ * \param a2
  *  The annotate two context.
  *
- * Retruns
- * -------
+ * @return
  *  0 on success, otherwise -1 on error.
  */
 int a2_shutdown ( struct annotate_two *a2 );
 
-/***************************************************************************
- * Status Commands
- **************************************************************************/
+//@}
 
-/* a2_err_msg
- * ----------
+/******************************************************************************/
+/**
+ * @name Status Commands
+ * These functions are for querying the annotate_two context.
+ */
+/******************************************************************************/
+
+//@{
+
+/** 
+ * a2_err_msg
  *
  * Returns the last error message ?
  * Not implemented yet.
  * What should it return? How should errors be handled?
  *
- * a2
- * --
+ * \param a2
  *  The annotate two context.
  *
- * Returns
- * -------
+ * @return
  *  0 on success, otherwise -1 on error.
  */
 int a2_err_msg ( struct annotate_two *a2 );
 
-/* a2_is_client_ready
- * ------------------
+/** 
+ * a2_is_client_ready
  *
- * This determines if the annotate two subsystem is ready to recieve
+ * This determines if the annotate two context is ready to recieve
  * another command.
  *
- * a2
- * --
+ * \param a2
  *  The annotate two context.
  *
- * Returns
- * -------
+ * @return
  *  1 if it is ready, 0 if it is not.
  */
 int a2_is_client_ready(struct annotate_two *a2);
 
-/* a2_user_ran_command
- * -------------------
+/** 
+ * a2_user_ran_command
  *
  * This lets the annotate_two know that the user ran a command.
  * The client can update itself here if it need to.
  *
- * a2
- * --
+ * \param a2
  *  The annotate two context.
  *
- *
- * Returns: -1 on error, 0 on success
+ * @return
+ * 	-1 on error, 0 on success
  */
 int a2_user_ran_command ( struct annotate_two *a2 );
 
-/* a2_prepare_for_command
- * ----------------------
+/** 
+ * a2_prepare_for_command
  *
  *  Prepare's the client for the command COM to be run.
  *
- * a2
- * --
+ * \param a2
  *  The annotate two context.
  *
- *  com:    The command to be run.
+ *  \param com
+ *  	The command to be run.
+ *
+ * @return
+ * 	-1 on error, 0 on success
  */
 int a2_prepare_for_command ( struct annotate_two *a2, struct command *com );
 
-/* a2_is_misc_prompt
- * -----------------
+/** 
+ * a2_is_misc_prompt
  *
  *  This is a hack. It should be removed eventually.
  *  It tells tgdb-base not to send its internal commands when this is true.
  *
- * a2
- * --
+ * \param a2
  *  The annotate two context.
  *
- * Returns
- * -------
+ * @return
  *  1 if it is at a misc prompt, 0 if it is not.
  */
 int a2_is_misc_prompt ( struct annotate_two *a2 );
 
-/***************************************************************************
- * Input/Output Commands
- **************************************************************************/
+//@}
 
- /* a2_parse_io
-  * -----------
+/******************************************************************************/
+/**
+ * @name Input/Output commands
+ * These functions are for communicating I/O with an annotate two context.
+ */
+/******************************************************************************/
+
+//@{
+
+ /** 
+  * a2_parse_io
   *
   * This recieves all of the output from the debugger. It is all routed 
   * through this function. 
   *
-  * a2
-  * --
+  * \param a2
   *  The annotate two context.
   *
-  * input_data
-  * ----------
+  * \param input_data
   *  This is the stdout from the debugger. This is the data that parse_io 
   *  will parse.
   *
-  * input_data_size
-  * ---------------
-  *  This is the size of debuggger_stdout
+  * \param input_data_size
+  *  This is the size of input_data.
   *
-  * debugger_output
-  * ---------------
+  * \param debugger_output
   *  This is an out variable. It contains data that has been determined to
   *  be the output of the debugger that the user should see.
   *
-  * debugger_output_size
-  * --------------------
+  * \param debugger_output_size
   *  This is the size of debugger_output
   *
-  * inferior_output
-  * ---------------
+  * \param inferior_output
   *  This is an out variable. It contains data that has been determined to
   *  be the output of the inferior that the user should see.
   *
-  * inferior_output_size
-  * --------------------
+  * \param inferior_output_size
   *  This is the size of inferior_output
   *
-  * q
-  * -
-  *  Any commands that the annotate_two subsystem has discovered will
+  * \param q
+  *  Any commands that the annotate_two context has discovered will
   *  be added to the queue Q. This will eventually update the client
   *  of the libtgdb library.
   *
-  * Returns
-  * -------
+  * @return
   *  0 on success, otherwise -1 on error.
   */
 int a2_parse_io ( 
@@ -277,156 +274,180 @@ int a2_parse_io (
 		char *inferior_output, size_t *inferior_output_size,
 		struct queue *q );
 
-/***************************************************************************
- * Functional Commands
- **************************************************************************/
+//@}
 
-/* a2_get_source_absolute_filename
- * ------------------------------------
+
+/******************************************************************************/
+/**
+ * @name Functional commands
+ * These functinos are used to ask an annotate_two context to perform a task.
+ */
+/******************************************************************************/
+
+//@{
+
+/** 
+ * a2_get_source_absolute_filename
  *
  *  Gets the Absolute path of FILE.
  *  
- *  a2
- *  --
+ *  \param a2
  *   The annotate two context.
  *
- *  file
- *  ----
+ *  \param file
  *   The relative path that gdb outputted.
+ *
+ * @return
+ *   0 on success, otherwise -1 on error.
  */
 int a2_get_source_absolute_filename ( struct annotate_two *a2, const char *file );
 
-/* a2_get_inferior_sources
- * -----------------------
+/** 
+ * a2_get_inferior_sources
  *
  *  Gets all the source files that the inferior makes up.
  *
- *  a2
- *  --
+ *  \param a2
  *   The annotate two context.
+ *
+ * @return
+ *   0 on success, otherwise -1 on error.
  */
 int a2_get_inferior_sources ( struct annotate_two *a2 );
 
-/* a2_change_prompt
- * ----------------
+/** 
+ * a2_change_prompt
  *
  *  This will change the prompt the user sees to PROMPT.
  *
- * a2
- * --
+ * \param a2
  *  The annotate two context.
  *
- * prompt
- * ------
+ * \param prompt
  *  The new prompt to change too.
+ *
+ * @return
+ *   0 on success, otherwise -1 on error.
  */
 int a2_change_prompt(struct annotate_two *a2, const char *prompt);
 
-/* a2_command_callback
- * ------------------------
+/** 
+ * a2_command_callback
  *
  * This is called when readline determines a command has been typed. 
  *
- * a2
- * --
+ * \param a2
  *  The annotate two context.
  *
- * command
- * ---- 
+ * \param command
  *  The command the user typed without the '\n'.
+ *
+ * @return
+ *   0 on success, otherwise -1 on error.
  */
 int a2_command_callback(struct annotate_two *a2, const char *command);
 
-/* a2_completion_callback
- * ---------------------------
+/** 
+ * a2_completion_callback
  *
  * This is called when readline determines a command needs to be completed.
  *
- * a2
- * --
+ * \param a2
  *  The annotate two context.
  *
- * command
- * ----
+ * \param command
  *  The command to be completed
+ *
+ * @return
+ *   0 on success, otherwise -1 on error.
  */
 int a2_completion_callback(struct annotate_two *a2, const char *command);
 
-/* a2_return_client_command
- * -----------------------------
+/** 
+ * a2_return_client_command
  *
  *  This returns the command to send to gdb for the enum C.
  *  It will return NULL on error, otherwise correct string on output.
  *
- * a2
- * --
- *  The annotate two context.
+ * \param a2
+ *  	The annotate two context.
  *
- * c
- * -
- *  The command to run.
+ * \param c
+ *  	The command to run.
+ *
+ * @return
+ *   	Command on success, otherwise NULL on error.
  */
 char *a2_return_client_command ( struct annotate_two *a2, enum tgdb_command c );
 
-/* a2_client_modify_breakpoint
- * --------------------------------
+/** 
+ * a2_client_modify_breakpoint
  *
- * a2
- * --
- *  The annotate two context.
+ * \param a2
+ * 	The annotate two context.
  *
- * Look at tgdb.h
+ * \param file
+ * 	The file to set the breakpoint in.
+ * \param line
+ * 	The line in FILE to set the breakpoint in.
+ * \param b
+ * 	Determines what the user wants to do with the breakpoint.
  *
+ * @return
+ * 	NULL on error or message to print to terminal
  */
 char *a2_client_modify_breakpoint ( struct annotate_two *a2, const char *file, int line, enum tgdb_breakpoint_action b );
 
-/* a2_get_debuger_pid
- * ------------------
+/** 
+ * a2_get_debuger_pid
  *
- * a2
- * --
+ * \param a2
  *  The annotate two context.
  *
- *  Returns -1 on error. Or pid on Success.
+ *  @return 
+ *  -1 on error. Or pid on Success.
  */
 pid_t a2_get_debugger_pid ( struct annotate_two *a2 );
 
-/***************************************************************************
- * Inferior tty commands
- **************************************************************************/
+//@}
 
-/* a2_open_new_tty
- * ---------------
+/******************************************************************************/
+/**
+ * @name Inferior tty commands
+ * These functinos are used to alter an annotate_two contexts tty state.
+ */
+/******************************************************************************/
+
+//@{
+
+/** 
+ * a2_open_new_tty
  *
- * a2
- * --
+ * \param a2
  *  The annotate two context.
  *
- * inferior_stdin
- * --------------
+ * \param inferior_stdin
  *  Writing to this descriptor, writes to the stdin of the inferior.
  *
- * inferior_stdout
- * ---------------
+ * \param inferior_stdout
  *  Reading from this descriptor, reads from the inferior's stdout.
  *
- * Retruns
- * -------
+ * @return
  *  0 on success, otherwise -1 on error.
  */
 int a2_open_new_tty ( struct annotate_two *a2, int *inferior_stdin, int *inferior_stdout );
 
-/* a2_get_tty_name
- * ---------------
+/** 
+ * a2_get_tty_name
  *
- * a2
- * --
+ * \param a2
  *  The annotate two context.
  *
- * Retruns
- * -------
- *  tty name on success, otherwise NULL on error.
+ * @return
+ * 	tty name on success, otherwise NULL on error.
  */
 char *a2_get_tty_name ( struct annotate_two *a2 );
+
+//@}
 
 #endif /* __A2_TGDB_H__ */
