7394438e249c71a670d63cb0a5d1f9c3c572032d
Bob Rossi <bob@brasko.net>
2003-09-18 02:18:41 +0000

    Removing iterator functions from queue. No one used the functionality.
    git-svn-id: https://cgdb.svn.sourceforge.net/svnroot/cgdb/cgdb/trunk@290 61d65b3c-fd13-0410-b770-9cb5f9339520

--- various/adt/include/queue.h
+++ various/adt/include/queue.h
@@ -10,7 +10,6 @@
 #endif /* HAVE_SYS_TYPES_H */
 
 struct queue;
-struct queue_iterator;
 
 typedef void (*item_func)(void *item);
 
@@ -53,67 +52,5 @@ void queue_traverse_list(struct queue *q, item_func func);
  * Returns          - The size of the list, 0 if head is NULL
  */
 int queue_size(struct queue *q);
-
-/* queue_iterator functions
- * -------------------------
- * 
- * As of now, the iterator functions only move down the queue.
- * They start at the front of the queue and move to the end.
- * If it is desirable to move backward, the queue will have to 
- * become a double linked list.
- * At that point, it might be worth creating a list.
- */
-
-/* queue_iterator_init:
- * --------------------
- *
- * Initialize a queue_iterator. This function does not fail.
- */
-struct queue_iterator *queue_iterator_init ( void );
-
-/* queue_iterator_init:
- * --------------------
- *  
- * finalize a queue_iterator.
- *
- *  i           - The iterator
- */
-void queue_iterator_free ( struct queue_iterator *i );
-
-/* queue_iterator_reset:
- * ---------------------
- * 
- *  Sets the iterator to the front of the queue.
- *
- *  q           - The queue to modify
- *  i           - The iterator
- */
-void queue_iterator_reset ( struct queue *q, struct queue_iterator *i );
-
-/* queue_iterator_forward:
- * -----------------------
- * 
- * Moves the iterator to the next item
- *
- *  i           - The iterator
- */
-void queue_iterator_forward ( struct queue_iterator *i );
-
-/* queue_iterator_end_of_list:
- *
- * Determines if the iterator can not move forward anymore.
- *
- *  i           - The iterator
- *  Return: 1 if end of list, 0 otherwise.
- */
-int queue_iterator_end_of_list ( struct queue_iterator *i );
-
-/* queue_iterator_get_item:
- * ------------------------
- *
- *  i           - The iterator
- * Returns the item, or null on error
- */
-void *queue_iterator_get_item ( struct queue_iterator *i );
 
 #endif /* __QUEUE_H__ */
