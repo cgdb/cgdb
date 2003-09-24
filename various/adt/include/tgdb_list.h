ef0c42a4e4809a8a9def8fa2daebfdb27841fff3
Bob Rossi <bob@brasko.net>
2003-09-24 00:45:01 +0000

    Add new doubly linked list data structure
    git-svn-id: https://cgdb.svn.sourceforge.net/svnroot/cgdb/cgdb/trunk@295 61d65b3c-fd13-0410-b770-9cb5f9339520

---
+++ various/adt/include/tgdb_list.h
@@ -0,0 +1,175 @@
+#ifndef __TGDB_LIST_H__
+#define __TGDB_LIST_H__
+
+struct tgdb_list;
+struct tgdb_list_iterator;
+
+typedef void (*item_func)(void *item);
+
+/* 
+ * Initializes a new empty list.
+ * 
+ * Returns
+ *    The new list.
+ */
+struct tgdb_list *tgdb_list_init ( void );
+
+/* 
+ * Appends item to the end of the list.
+ * 
+ * tlist
+ *    The list to append an item to.
+ *
+ * item
+ *    The item to add to the list
+ */
+void tgdb_list_append ( struct tgdb_list *tlist, void *item );
+
+/* 
+ * Prepends item to the beggining of the list.
+ * 
+ * tlist
+ *    The list to prepend an item to.
+ *
+ * item
+ *    The item to add to the list
+ */
+void tgdb_list_prepend ( struct tgdb_list *tlist, void *item );
+
+/* 
+ * appends the item after the position of the iterator.
+ *
+ * tlist
+ *    The list to add an item to
+ * 
+ * i
+ *    The iterator to insert after.
+ *
+ * item
+ *    The item to add to the list
+ */
+void tgdb_list_insert_after ( 
+		struct tgdb_list *tlist, 
+		struct tgdb_list_iterator *i, 
+		void *item );
+
+/* 
+ * prepands the item before the position of the iterator.
+ * 
+ * tlist
+ *    The list to add an item to
+ * 
+ * i
+ *    The iterator to insert before.
+ *
+ * item
+ *    The item to add to the list
+ */
+void tgdb_list_insert_before ( 
+		struct tgdb_list *tlist,
+		struct tgdb_list_iterator *i, 
+		void *item );
+
+/* Traversing the tree */
+
+/* 
+ * Traverses each item in the list, passing each element to func
+ *
+ * tlist
+ *    The list to traverse
+ *
+ * func
+ *    The function to call on each item
+ */
+void tgdb_list_foreach ( struct tgdb_list *tlist, item_func func );
+
+/* Freeing the tree */
+
+/* 
+ * Free's list item by calling func on each element
+ *
+ * After this function is called, the list is empty.
+ *
+ * tlist
+ *    The list to free
+ *
+ * func
+ *    The function to free an item
+ */
+void tgdb_list_free ( struct tgdb_list *tlist, item_func func );
+
+/* 
+ * Gets the size of the list.
+ *
+ * tlist
+ *    The list context to get the size of.
+ *
+ * Returns
+ * 	  The size of the list, 0 if empty.
+ */
+int tgdb_list_size ( struct tgdb_list *tlist );
+
+/* Moving through the list */
+
+/*
+ * Gets a hold of the first iterator in the list
+ *
+ * tlist
+ *    The list to get the beggining of
+ *
+ * i
+ *    The iterator to assign at the first position of the list
+ *
+ * Returns
+ * 	  1 if was available, 0 otherwise
+ */
+int tgdb_list_get_first ( struct tgdb_list *tlist, struct tgdb_list_iterator *i );
+
+/*
+ * Gets a hold of the last iterator in the list
+ *
+ * tlist
+ *    The list to get the last iterator.
+ *
+ * i
+ *    The iterator to assign at the last position of the list
+ *    
+ * Returns
+ * 	  1 if was available, 0 otherwise
+ */
+int tgdb_list_get_last ( struct tgdb_list *tlist, struct tgdb_list_iterator *i );
+
+/* 
+ * Moves the iterator one step forward through the list
+ *
+ * i
+ *    The iterator to advance.
+ *    
+ * Returns
+ *    1 if successful, otherwise 0
+ */
+int tgdb_list_next ( struct tgdb_list_iterator *i );
+
+/* 
+ * Moves the iterator one step backwards through the list
+ *
+ * i
+ *    The iterator to move backwards.
+ *
+ * Returns
+ *    1 if successful, otherwise 0
+ */
+int tgdb_list_previous ( struct tgdb_list_iterator *i );
+
+/*
+ * Gets the data at the iterator's position.
+ *
+ * i
+ *    The iterator to get the item at.
+ *
+ * return
+ *    The item at the iterator, or NULL on error
+ */
+void *tgdb_get_item ( struct tgdb_list_iterator *i );
+
+#endif /* __TGDB_LIST_H__ */
---
+++ various/adt/src/tgdb_list.c
@@ -0,0 +1,313 @@
+#include "tgdb_list.h"
+#include "sys_util.h"
+#include "error.h"
+
+struct tgdb_list_node {
+	void *data;
+	struct tgdb_list_node *next;
+	struct tgdb_list_node *prev;
+};
+
+struct tgdb_list {
+	int size;
+	struct tgdb_list_node *head;
+	struct tgdb_list_node *tail;
+};
+
+struct tgdb_list_iterator {
+	struct tgdb_list_node *node;
+};
+
+struct tgdb_list *tgdb_list_init ( void ) {
+	struct tgdb_list *list;
+
+	list = (struct tgdb_list *)xmalloc ( sizeof ( struct tgdb_list ) );
+
+	/* Initialize all data members */
+	list->size = 0;
+	list->head = NULL;
+	list->tail = NULL;
+
+	return list;
+}
+
+/*
+ * Can insert any node between 2 other nodes.
+ *
+ * list
+ *    This is the current list we are inserting into.
+ *
+ * before
+ *    The node we want to insert after.
+ *
+ * after
+ *    The node to insert before.
+ *
+ * new_node
+ *    The node to insert into the list.
+ */
+static void tgdb_list_insert ( 
+	struct tgdb_list *list,
+	struct tgdb_list_node *before,
+	struct tgdb_list_node *after,
+	struct tgdb_list_node *new_node ) {
+
+	/* Do nothing if node or list is NULL */
+	if ( !new_node || !list)
+		return;
+
+	/* Inserting first item into list */
+	if ( before == NULL && after == NULL ) {
+		list->head = new_node;
+		new_node->next = NULL;
+		new_node->prev = NULL;
+
+	/* Insert at beggining of list */
+	} else if ( before == NULL ) {
+		new_node->next = list->head;
+		new_node->prev = NULL;
+
+		list->head->prev = new_node;
+
+		list->head = new_node;
+
+	/* Special case, insert into tail with list size 1 */
+	} else if ( after == NULL && list->size == 1 ) {
+		new_node->next = NULL;
+		new_node->prev = list->head;
+
+		list->head->next = new_node;
+
+		list->tail = new_node;
+
+	/* Insert at end of list */
+	} else if ( after == NULL ) {
+
+		new_node->next = NULL;
+		new_node->prev = list->tail;
+
+		list->tail->next = new_node;
+
+		list->tail = new_node;
+
+	/* Insert into middle of list */
+	} else {
+		new_node->next = before->next;
+		new_node->prev = after->prev;
+
+		before->next = new_node;
+		before->prev = new_node;
+	}
+
+	list->size++;
+}
+
+/*
+ * Can delete any node out of the tree
+ *
+ * list
+ *    This is the current list we are deleting from
+ *
+ * node
+ *    The node to delete
+ */
+static void tgdb_list_delete (
+		struct tgdb_list *list,
+		struct tgdb_list_node *node ) {
+
+	/* Do nothing if node or list is NULL */
+	if ( !node || !list)
+		return;
+	
+	/* Deleting from an empty list */
+	if ( tgdb_list_size ( list ) == 0) {
+
+		free ( node->data );
+		node->next = NULL;
+		node->prev = NULL;
+		node       = NULL;
+
+		list->head = NULL;
+		list->tail = NULL;
+
+	/* Deleting last element in the list */
+	} else if ( tgdb_list_size ( list ) == 1 ) {
+		/* Only the head is populated, free it */
+		free ( node->data );
+		node->next = NULL;
+		node->prev = NULL;
+		node       = NULL;
+
+		list->head = NULL;
+		list->tail = NULL;
+
+	/* Deleting from beggining of list */
+	} else if ( node->prev == NULL ) {
+		node->next->prev = NULL;
+		list->head = node->next;
+
+		free ( node->data );
+		node->next = NULL;
+		node->prev = NULL;
+		node       = NULL;
+
+		/* If the list is size 2, remove the tail and set only the head */
+		if ( tgdb_list_size ( list ) == 2 )
+			list->tail = NULL;
+
+	/* Delete from end of list */
+	} else if ( node->next == NULL ) {
+
+		node->prev->next = NULL;
+		list->tail = node->prev;
+
+		free ( node->data );
+		node->next = NULL;
+		node->prev = NULL;
+		node       = NULL;
+
+	/* Delete from middle of list */
+	} else {
+		node->prev->next = node->next;
+		node->next->prev = node->prev;
+
+		free ( node->data );
+		node->next = NULL;
+		node->prev = NULL;
+		node       = NULL;
+	}
+
+	list->size--;
+}
+
+static struct tgdb_list_node* tgdb_list_new_node ( void ) {
+	struct tgdb_list_node *node;
+	node = (struct tgdb_list_node *)xmalloc ( sizeof ( struct tgdb_list_node ));
+
+	node->data = (void*) NULL;
+	node->next = NULL;
+	node->prev = NULL;
+
+	return node;
+}
+
+void tgdb_list_append ( struct tgdb_list *tlist, void *item ) {
+	struct tgdb_list_node *node = tgdb_list_new_node ();
+
+	node->data = item;
+
+	tgdb_list_insert ( tlist, tlist->tail, NULL, node );	
+}
+
+void tgdb_list_prepend ( struct tgdb_list *tlist, void *item ) {
+	struct tgdb_list_node *node = tgdb_list_new_node ();
+
+	node->data = item;
+
+ 	tgdb_list_insert ( tlist, NULL, tlist->head, node );	
+}
+
+void tgdb_list_insert_after ( 
+		struct tgdb_list *tlist, 
+		struct tgdb_list_iterator *i, 
+		void *item ) {
+	
+	struct tgdb_list_node *node = tgdb_list_new_node ();
+
+	node->data = item;
+
+	tgdb_list_insert ( tlist, i->node, i->node->next, node );
+}
+
+void tgdb_list_insert_before ( 
+		struct tgdb_list *tlist,
+		struct tgdb_list_iterator *tlisti, 
+		void *item ) {
+
+	struct tgdb_list_node *node = tgdb_list_new_node ();
+
+	node->data = item;
+
+	tgdb_list_insert ( tlist, tlisti->node->prev, tlisti->node, node );
+}
+
+void tgdb_list_foreach ( struct tgdb_list *tlist, item_func func ) {
+	struct tgdb_list_iterator i;
+	int val;
+
+	val = tgdb_list_get_first ( tlist, &i );
+
+	while ( val != 0 ) {
+		func ( i.node->data );
+		val = tgdb_list_next ( &i );
+	}
+}
+
+
+
+void tgdb_list_free ( struct tgdb_list *tlist, item_func func ) {
+	struct tgdb_list_iterator i;
+	int val;
+
+	val = tgdb_list_get_first ( tlist, &i );
+
+	while ( val != 0 ) {
+		func ( i.node->data );
+		tgdb_list_delete ( tlist, i.node->data );
+		val = tgdb_list_next ( &i );
+	}
+}
+
+int tgdb_list_size ( struct tgdb_list *tlist ) {
+	if ( tlist ) 
+		return tlist->size;
+
+	return 0;
+}
+
+int tgdb_list_get_first ( struct tgdb_list *tlist, struct tgdb_list_iterator *i ) {
+	if ( tlist ) {
+		i->node=tlist->head;
+		return 1;
+	}
+
+	return 0;
+}
+
+int tgdb_list_get_last ( struct tgdb_list *tlist, struct tgdb_list_iterator *i ) {
+	if ( tlist ) {
+		if ( tlist->size == 1 )
+			i->node = tlist->head;
+		else
+			i->node = tlist->tail;
+
+		return 1;
+	}
+
+	return 0;
+}
+
+int tgdb_list_next ( struct tgdb_list_iterator *i ) {
+	if ( i ) {
+		i->node = i->node->next;
+		return 1;
+	}
+
+	return 0;
+}
+
+int tgdb_list_previous ( struct tgdb_list_iterator *i ) {
+	if ( i ) {
+		i->node = i->node->prev;
+		return 1;
+	}
+
+	return 0;
+}
+
+void *tgdb_get_item ( struct tgdb_list_iterator *i ) {
+	if ( i )
+		return i->node->data;
+
+	return (void*)NULL;
+}
