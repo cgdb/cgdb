b50c04847b222f7289ce3dab13ed408572424b36
Bob Rossi <bob@brasko.net>
2004-08-26 18:34:02 +0000

    A modified grammar, capable of parsing several GDB 'output' productions.
    git-svn-id: https://cgdb.svn.sourceforge.net/svnroot/cgdb/cgdb/trunk@434 61d65b3c-fd13-0410-b770-9cb5f9339520

--- tgdb/gdbmi/src/gdbmi_grammar.y
+++ tgdb/gdbmi/src/gdbmi_grammar.y
@@ -38,6 +38,7 @@ output_ptr syntax_tree;
 %token GDB 				/* (gdb) */
 
 %union {
+	struct output *u_output;
 	struct oob_record *u_oob_record;
 	struct result_record *u_result_record;
 	int u_result_class;
@@ -55,13 +56,16 @@ output_ptr syntax_tree;
 	int u_stream_record_kind;
 }
 
+%type <u_output> opt_output_list
+%type <u_output> output_list
+%type <u_output> output
 %type <u_oob_record> oob_record
-%type <u_oob_record> oob_record_list
+%type <u_oob_record> opt_oob_record_list
 %type <u_result_record> opt_result_record
 %type <u_result_record> result_record
 %type <u_result_class> result_class
 %type <u_async_record_kind> async_record_class
-%type <u_result> result_list_prime
+%type <u_result> opt_result_list
 %type <u_result> result_list
 %type <u_result> result
 %type <u_token> opt_token
@@ -73,28 +77,47 @@ output_ptr syntax_tree;
 %type <u_variable> variable
 %type <u_value> value
 %type <u_value> value_list
-%type <u_value> value_list_prime
 %type <u_tuple> tuple
 %type <u_list> list
 %type <u_stream_record_kind> stream_record_class
 
 
-%start output
+%start opt_output_list
 
 %%
 
-output: oob_record_list opt_result_record GDB NEWLINE { 
+opt_output_list: {
+};
+
+opt_output_list: output_list {
+	$$ = $1;	
+	printf ( "Parser passed\n" );
+};
+
+output_list: output {
+	$$ = $1;
+};
+
+output_list: output_list output {
+	$$ = append_to_list ( $1, $2 );
+};
+
+output: opt_oob_record_list opt_result_record GDB NEWLINE { 
 	syntax_tree = malloc ( sizeof ( struct output ) );
 	syntax_tree->oob_record = $1;
 	syntax_tree->result_record = $2;
 	printf ("VALID\n" ); 
 } ;
 
-oob_record_list: oob_record_list oob_record NEWLINE {
+opt_oob_record_list: {
+	$$ = NULL;
+};
+
+opt_oob_record_list: opt_oob_record_list oob_record NEWLINE {
 	$$ = append_to_list ( $1, $2 );
 };
 
-oob_record_list: {
+opt_result_record: {
 	$$ = NULL;
 };
 
@@ -102,15 +125,18 @@ opt_result_record: result_record NEWLINE {
 	$$ = $1;
 };
 
-opt_result_record: {
-	$$ = NULL;
-};
-
-result_record: opt_token CARROT result_class result_list_prime {
+result_record: opt_token CARROT result_class {
 	$$ = malloc ( sizeof ( struct result_record ) );
 	$$->token = $1;
 	$$->result_class = $3;
-	$$->result = $4;
+	$$->result = NULL;
+};
+
+result_record: opt_token CARROT result_class COMMA opt_result_list {
+	$$ = malloc ( sizeof ( struct result_record ) );
+	$$->token = $1;
+	$$->result_class = $3;
+	$$->result = $5;
 };
 
 oob_record: async_record {
@@ -144,10 +170,10 @@ async_record_class: EQUAL_SIGN {
 	$$ = GDBMI_NOTIFY;	
 };
 
-async_output: async_class result_list_prime {
+async_output: async_class COMMA opt_result_list {
 	$$ = malloc ( sizeof ( struct async_output ) );
 	$$->async_class = $1;
-	$$->result_ptr = $2;
+	$$->result_ptr = $3;
 };
 
 result_class: DONE {
@@ -174,20 +200,20 @@ async_class: STOPPED {
 	$$ = GDBMI_STOPPED;
 };
 
-result_list_prime: result_list {
+opt_result_list: {
+	$$ = NULL;
+};
+
+opt_result_list: result_list {
 	$$ = $1;
 };
 
-result_list_prime: {
-	$$ = NULL;
+result_list: result {
+	$$ = append_to_list ( NULL, $1 );	
 };
 
 result_list: result_list COMMA result {
 	$$ = append_to_list ( $1, $3 );
-};
-
-result_list: COMMA result {
-	$$ = append_to_list ( NULL, $2 );	
 };
 
 result: variable EQUAL_SIGN value {
@@ -200,20 +226,12 @@ variable: STRING_LITERAL {
 	$$ = strdup ( gdbmi_text );
 };
 
-value_list_prime: value_list {
-	$$ = $1;
-};
-
-value_list_prime: {
-	$$ = NULL;
+value_list: value {
+	$$ = append_to_list ( NULL, $1 );	
 };
 
 value_list: value_list COMMA value {
 	$$ = append_to_list ( $1, $3 ); 
-};
-
-value_list: COMMA value {
-	$$ = append_to_list ( NULL, $2 );	
 };
 
 value: CSTRING {
@@ -238,28 +256,28 @@ tuple: OPEN_BRACE CLOSED_BRACE {
 	$$ = NULL;
 };
 
-tuple: OPEN_BRACE result result_list_prime CLOSED_BRACE {
+tuple: OPEN_BRACE result_list CLOSED_BRACE {
 	$$ = malloc ( sizeof ( struct tuple ) );
 	/* Order probably matters here */
-	$$->result = append_to_list ( $3, $2 );
+	$$->result = $2;
 };
 
 list: OPEN_BRACKET CLOSED_BRACKET {
 	$$ = NULL;
 };
 
-list: OPEN_BRACKET value value_list_prime CLOSED_BRACKET {
+list: OPEN_BRACKET value_list CLOSED_BRACKET {
 	$$ = malloc ( sizeof ( struct list ) );
 	/* Order probably matters here */
 	$$->list = GDBMI_VALUE;
-	$$->variant.value = append_to_list ( $3, $2 );
+	$$->variant.value = $2;
 };
 
-list: OPEN_BRACKET result result_list_prime CLOSED_BRACKET {
+list: OPEN_BRACKET result_list CLOSED_BRACKET {
 	$$ = malloc ( sizeof ( struct list ) );
 	/* Order probably matters here */
 	$$->list = GDBMI_RESULT;
-	$$->variant.result = append_to_list ( $3, $2 );
+	$$->variant.result = $2;
 };
 
 stream_record: stream_record_class CSTRING {
@@ -280,12 +298,12 @@ stream_record_class: AMPERSAND {
 	$$ = GDBMI_LOG;
 };
 
-opt_token: token {
-	$$ = $1;
-};
-
 opt_token: {
 	$$ = -1;	
+};
+
+opt_token: token {
+	$$ = $1;
 };
 
 token: INTEGER_LITERAL {
--- tgdb/gdbmi/src/gdbmi_il.h
+++ tgdb/gdbmi/src/gdbmi_il.h
@@ -15,6 +15,9 @@ typedef struct value *value_ptr;
 typedef struct tuple *tuple_ptr;
 typedef struct list *list_ptr;
 
+/**
+ * A kind of result's that GDB is capable of producing.
+ */
 enum result_class {
 	GDBMI_DONE,
 	GDBMI_RUNNING,
@@ -23,45 +26,135 @@ enum result_class {
 	GDBMI_EXIT
 };
 
+/**
+ * This is the root of a parsed GDB/MI Output command.
+ */
 struct output {
+	/**
+	 * Every output command has a list of optional oob_record's.
+	 * This will be the head of the list, otherwise NULL.
+	 */
 	oob_record_ptr oob_record;
+
+	/**
+	 * Every output command has an optional result_record.
+	 * list, otherwise NULL.
+	 */
 	result_record_ptr result_record;
 };
 
+/**
+ * A result record represents the result of a command sent to GDB.
+ */
 struct result_record {
+	/**
+	 * This is the unique identifier that was passed to GDB when
+	 * asking for a command to be done on behalf of the front end.
+	 */
 	token_t token;
+
+	/** The kind of result that this class represents */
 	enum result_class result_class;
+
+	/** The results of the command. */
 	result_ptr result;
 };
 
+/**
+ * There are several kinds of output that GDB can send.
+ *
+ * \GDBMI_ASYNC
+ * I believe that the asyncronous records contain data that was not asked
+ * for by the front end. An asyncronous event occured within the inferior
+ * or GDB and GDB needs to update the front end.
+ *
+ * For instance, I believe this is useful for when breakpoints are modified, 
+ * instead of having the front end ask if the breakpoints were modified 
+ * every time.
+ *
+ * \GDBMI_STREAM
+ * This is the result of normal output from the console, target or GDB.
+ */
 enum oob_record_kind {
 	GDBMI_ASYNC,
 	GDBMI_STREAM
 };
 
+/**
+ * This is an out of band record.
+ */
 struct oob_record {
+	/**
+	 * This is the kind of oob_record
+	 */
 	enum oob_record_kind record;
 	union {
+		 /** If it's an GDBMI_ASYNC record */
 		async_record_ptr async_record;
+		 /** If it's an GDBMI_STREAM record */
 		stream_record_ptr stream_record;
 	} variant;
+
+	/** A pointer to the next oob_record */
+	struct oob_record *next;
 };
 
+/**
+ * This represents each kind of asyncronous record GDB is capable of sending. 
+ */
 enum async_record_kind {
+	/** 
+	 * contains on-going status information about the progress 
+	 * of a slow operation. It can be discarded. 
+	 */
 	GDBMI_STATUS,
+	/** 
+	 * contains asynchronous state change on the target 
+	 * (stopped, started, disappeared). 
+	 */
 	GDBMI_EXEC,
+	
+	/** 
+	 * contains supplementary information that the client should handle 
+	 * (e.g., a new breakpoint information). 
+	 */
 	GDBMI_NOTIFY
 };
 
+/**
+ * This represents each kind of stream record GDB is capable of sending. 
+ */
 enum stream_record_kind {
+	/** 
+	 * Output that should be displayed as is in the console. 
+	 * It is the textual response to a CLI command. 
+	 */
 	GDBMI_CONSOLE,
+	/** 
+	 * Output produced by the target program. 
+	 */
 	GDBMI_TARGET,
+	/** 
+	 * Output text coming from GDB's internals, for instance messages 
+	 * that should be displayed as part of an error log. 
+	 */
 	GDBMI_LOG
 };
 
+/**
+ * An asyncronous record
+ */
 struct async_record {
+	/**
+	 * This is the unique identifier that was passed to GDB when
+	 * asking for a command to be done on behalf of the front end.
+	 */
 	token_t token;
+
+	/** The kind of asyncronous record this represents */
 	enum async_record_kind async_record;
+
+	/** The output of the asynchronous record */
 	async_output_ptr async_output;
 };
 
@@ -74,9 +167,18 @@ struct async_output {
 	result_ptr result_ptr;
 };
 
+/**
+ * The result from GDB. This is a linked list.
+ * If the result is a key/value pair, then 'variable' is the key
+ * and 'value' is the value.
+ */
 struct result {
+	/** Key */
 	const char *variable;
+	/** Value */
 	value_ptr value;
+	/** Pointer to the next result */
+	struct result *next;
 };
 
 enum value_kind {
@@ -87,15 +189,19 @@ enum value_kind {
 
 struct value {
 	enum value_kind value_kind;
+
 	union {
 		const char *cstring;
 		tuple_ptr tuple;
 		list_ptr list;
 	} variant;
+
+	struct value *next;
 };
 
 struct tuple {
 	result_ptr result;
+	struct tuple *next;
 };
 
 enum list_kind {
@@ -111,6 +217,8 @@ struct list {
 		value_ptr value;
 		result_ptr result;
 	} variant;
+
+	struct list *next;
 };
 
 struct stream_record {
@@ -118,6 +226,23 @@ struct stream_record {
 	const char *cstring;
 };
 
-void* append_to_list ( void *list, void*item );
+int print_result_class ( enum result_class param );
 
+output_ptr create_output ( void );
+int destory_output_ptr ( output_ptr param );
+int print_output_ptr ( output_ptr param );
+
+result_record_ptr create_result_record ( void );
+int destroy_result_record ( result_record_ptr param );
+int print_result_record ( result_record_ptr param );
+
+int print_oob_record_kind ( enum oob_record_kind param );
+
+oob_record_ptr create_oob_record ( void );
+int destroy_oob_record ( oob_record_ptr param );
+int print_oob_record ( oob_record_ptr param );
+
+int print_async_record_kind ( enum async_record_kind param );
+
+void* append_to_list ( void *list, void*item );
 #endif
