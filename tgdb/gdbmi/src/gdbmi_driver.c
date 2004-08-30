70b41d3cacd80762f76f52b093c25765bde8b88e
Bob Rossi <bob@brasko.net>
2004-08-30 02:32:27 +0000

    Compiles under Cygwin.
    git-svn-id: https://cgdb.svn.sourceforge.net/svnroot/cgdb/cgdb/trunk@438 61d65b3c-fd13-0410-b770-9cb5f9339520

--- tgdb/gdbmi/src/gdbmi_driver.c
+++ tgdb/gdbmi/src/gdbmi_driver.c
@@ -16,7 +16,7 @@ static void usage ( char *progname ) {
 extern FILE *gdbmi_in;
 extern int gdbmi_lex ( void );
 extern char *gdbmi_text;
-int gdbmi_get_lineno  (void);
+extern int gdbmi_lineno;
 extern output_ptr tree;
 
 int instrument_lexer ( void ) {
@@ -104,10 +104,10 @@ int main ( int argc, char **argv ) {
 
 void gdbmi_error (const char *s) { 
 	printf ( "Error: %s ", s );
-	if ( strcmp ( gdbmi_text, "\n" ) == 0 )
-		printf ( "at end of line %d\n", gdbmi_get_lineno() );
-	else {
-		printf ( "at token(%s), line (%d)\n", gdbmi_text, gdbmi_get_lineno() );
+	if ( strcmp ( gdbmi_text, "\n" ) == 0 ) {
+		printf ( "at end of line %d\n", gdbmi_lineno );
+	} else {
+		printf ( "at token(%s), line (%d)\n", gdbmi_text, gdbmi_lineno );
 		gdbmi_lex();
 		printf ( "before (%s)\n", gdbmi_text );
 	}
