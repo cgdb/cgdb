7d6424c742b252044259bf04ff6b25f47211796f
Bob Rossi <bob@brasko.net>
2006-01-24 02:26:17 +0000

    Update Copyright.
    git-svn-id: https://cgdb.svn.sourceforge.net/svnroot/cgdb/cgdb/trunk@625 61d65b3c-fd13-0410-b770-9cb5f9339520

--- cgdb/src/cgdb.c
+++ cgdb/src/cgdb.c
@@ -332,7 +332,7 @@ printf(
 static char *version_info(void) {
     static char buf[MAXLINE];
     sprintf(buf, "%s %s\r\n%s", "CGDB", VERSION, 
-            "Copyright 2002-2005 Bob Rossi and Mike Mueller.\n"
+            "Copyright 2002-2006 Bob Rossi and Mike Mueller.\n"
             "CGDB is free software, covered by the GNU General Public License, and you are\n"
             "welcome to change it and/or distribute copies of it under certain conditions.\n"
             "There is absolutely no warranty for CGDB.\n");
