0544c87cfce9263339a6ec1621869d9b119b7d72
Bob Rossi <bob@brasko.net>
2006-02-04 19:37:04 +0000

    Oops, forgot to add these.
    git-svn-id: https://cgdb.svn.sourceforge.net/svnroot/cgdb/cgdb/trunk@676 61d65b3c-fd13-0410-b770-9cb5f9339520

---
+++ cgdb/include/usage.h
@@ -0,0 +1,9 @@
+#ifndef __USAGE_H__
+#define __USAGE_H__
+
+/**
+ * This will generate the usage message for CGDB.
+ */
+void usage (void);
+
+#endif /* __USAGE_H__ */
---
+++ cgdb/src/usage.c
@@ -0,0 +1,31 @@
+#if HAVE_CONFIG_H
+#include "config.h"
+#endif /* HAVE_CONFIG_H */
+
+#if HAVE_STDIO_H
+#include <stdio.h> 
+#endif /* HAVE_STDIO_H */
+
+#include "usage.h"
+
+void 
+usage (void)
+{
+printf( 
+"CGDB Usage:\n"      
+"   cgdb [cgdb options] [gdb options]\n"
+"\n"
+"CGDB Options:\n"
+#ifdef HAVE_GETOPT_H
+"   --version   Print version information and then exit.\n"
+#else
+"   -v          Print version information and then exit.\n"
+#endif
+#ifdef HAVE_GETOPT_H
+"   --help      Print help (this message) and then exit.\n"
+#else
+"   -h          Print help (this message) and then exit.\n"
+#endif
+"   -d          Set debugger to use.\n"
+    );
+}
