4515946e8275b4ba5d3a2eca65a9012f1cd81219
Bob Rossi <bob@brasko.net>
2006-06-01 02:16:03 +0000

    More auto release fixes.
    git-svn-id: https://cgdb.svn.sourceforge.net/svnroot/cgdb/cgdb/trunk@794 61d65b3c-fd13-0410-b770-9cb5f9339520

--- autoregen.sh
+++ autoregen.sh
@@ -3,6 +3,17 @@
 rm -rf autom4te.cache/
 
 ################################################################################
+echo "-- Update configure.in to reflect the new version number"
+################################################################################
+if [ "$1" = "" ]; then
+  CGDB_VERSION=`date +%Y%m%d`
+else
+  CGDB_VERSION=$1
+fi
+
+perl -pi -e "s/AC_INIT\(cgdb, (.*)\)/AC_INIT\(cgdb, $CGDB_VERSION\)/g" configure.in
+
+################################################################################
 echo "-- Running aclocal"
 ################################################################################
 aclocal -I config
