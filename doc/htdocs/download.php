540672af3c0ca83e665d1bc986252c2bb980e547
Mike Mueller <mike@subfocal.net>
2011-09-05 23:30:41 -0700

    Simplify the download page.

--- doc/htdocs/download.php
+++ doc/htdocs/download.php
@@ -5,163 +5,39 @@
     include "header.php";
 ?>
 
-    <h3>Downloading CGDB</h3>
+    <h3>Downloading cgdb</h3>
 
-    <div class="pagetext">
-      <p>Choose your preferred installation mechanism:</p>
-      <ul>
-        <li>
-          <a href="#packages">Binary packages (platform-specific)</a>
-        </li>
-        <li>
-          <a href="#tarball">Source tarball</a>
-        </li>
-        <li>
-          <a href="#git">Git repository</a>
-        </li>
-      </ul>
-    </div>
-
-    <a name="packages"></a>
     <h4>Binary Packages</h4>
-
     <div class="pagetext">
-      <p>
-        The following distributions offer native packages for CGDB.
-        If you know of another distribution that packages CGDB, please let us
-        know so we can update this page.
-      </p>
+    <p>
+      <b>Unix:</b> Normally you can install a stable version of cgdb through
+      your distribution's packaging system.  Packages are available in all
+      major Linux distributions (Arch, CentOS, Debian, Fedora, Gentoo, RedHat,
+      Ubuntu, and probably others), as well as in the MacPorts system for
+      MacOS X.
+    </p>
+    <p>
+      <b>Windows binary:</b> <a href="files/cgdb-<?= LATEST ?>-win32.tar.gz"
+      >cgdb-<?= LATEST ?>-win32.tar.gz</a>.
+    </p>
+    <p>
+      This is a Cygwin executable that will run in a command window
+      or a Cygwin terminal.  Cygwin users may need to delete the DLLs in
+      the 'bin' directory.
+    </p>
     </div>
 
-    <!-- Nested pagetext divs as kind of a hack to get the whole page
-         indented below this point -->
-    <div class="pagetext">
-
-      <h5>Debian</h5>
-
-      <div class="pagetext">
-        <p>
-          Debian's package system contains a
-          <a href="http://packages.qa.debian.org/c/cgdb.html">CGDB package</a>,
-          maintained by
-          <a href="http://www.semistable.com/debian.html">Robert Lemmen</a>.
-          The stable version is fairly recent (0.6.3 as of this writing),
-          testing will get you the latest (<?= LATEST ?>).
-        </p>
-
-        <p>
-          To install, as root:
-          <code>apt-get install cgdb</code>
-        </p>
-      </div>
-
-      <h5>Ubuntu</h5>
-
-      <div class="pagetext">
-        <p>
-          See Debian instructions, or if you prefer aptitude:
-          <code>sudo aptitude install cgdb</code>
-        </p>
-      </div>
-
-      <h5>Gentoo</h5>
-
-      <div class="pagetext">
-        <p>
-          Portage generally has a relatively recent version of CGDB (<a
-          href="http://www.gentoo-portage.com/dev-util/cgdb">dev-util/cgdb</a>).
-          As of this writing, 0.6.2 is unmasked, but 0.6.3 is masked with
-          ~keyword.  (They're both stable, Gentoo just doesn't keep up so well.)
-        </p>
-
-        <p>
-          To install, as root:
-          <code>emerge -av cgdb</code>
-        </p>
-
-        <p>
-          We also provide a 
-          <a href="files/cgdb-<?= LATEST ?>.ebuild">current Gentoo ebuild</a>
-          (<?= LATEST ?>)
-          that you can use instead.  If you've never installed an ebuild before,
-          check out the <a href=
-          "http://gentoo-wiki.com/HOWTO_Installing_3rd_Party_Ebuilds">Gentoo
-          Wiki on 3rd party ebuilds</a>.  Once you overlay the new ebuild,
-          install cgdb with the command above.
-        </p>
-      </div>
-
-      <h5>Fedora</h5>
-
-      <div class="pagetext">
-        <p>
-          Fedora's extra repository has an up-to-date CGDB package.
-          To install it, as root:
-          <code>yum install cgdb</code>
-        </p>
-      </div>
-
-      <h5>Red Hat/CentOS (5.0)</h5>
-
-      <div class="pagetext">
-        <p>
-          <a href="http://fedoraproject.org/wiki/EPEL">Fedora EPEL</a>
-          repository has an up-to-date CGDB package. To install it, add
-          <a href="http://fedoraproject.org/wiki/EPEL/FAQ#howtouse">EPEL
-          support</a> to your existing RedHat/CentOS installation, then
-          install as you would in Fedora (above).
-        </p>
-      </div>
-
-      <h5>Windows</h5>
-
-      <div class="pagetext">
-        <p>
-          Windows users can use this
-          <a href="files/cgdb-<?= LATEST ?>-win32.tar.gz">win32 binary</a>,
-          which will work in a command window or a Cygwin terminal.
-          For Cygwin users, you may need to delete the DLLs in the
-          'bin' directory.
-        </p>
-      </div>
-
-      <h5>MacOS X</h5>
-
-      <div class="pagetext">
-        <p>
-          You can get a CGDB Mac PPC binary by using Jens Frederich's prebuilt
-          <a href="files/cgdb-<?= LATEST ?>-osx-universal.tar.gz">universal
-          binary</a>, also available as a
-          <a href="files/cgdb-<?= LATEST ?>-osx-universal.dmg">disk image</a>.
-          If you would like to contact him, his address is jfrederich (at)
-          gmail (dot) com.  Thanks Jens!
-        </p>
-
-        <p>
-          Alternatively, CGDB may be installed via the
-          <a href="http://www.macports.org/">MacPorts</a> package system.
-          Install MacPorts, then run:
-          <code>
-            sudo port install cgdb
-          </code>
-        </p>
-      </div>
-    </div>
-
-    <a name="tarball"></a>
-    <h4>Install from Sources</h4>
+    <h4>Source Tarball</h4>
 
     <div class="pagetext">
       <h5>Prerequisites</h5>
-
       <ol>
         <li>
           <a
           href="http://tiswww.case.edu/~chet/readline/rltop.html">readline</a>
           5.1 or greater.  An important design problem was fixed in version
-          5.1, any older version will not support CGDB.
+          5.1, any older version will not support cgdb.
         </li>
-
         <li>
           <a
           href="http://www.gnu.org/software/ncurses/ncurses.html">ncurses</a>
@@ -171,11 +47,9 @@
       </ol>
 
       <h5>Stable Release:</h5>
-
       <div class="pagetext">
         <p>
         <a href="http://prdownloads.sourceforge.net/cgdb/cgdb-<?= LATEST ?>.tar.gz?download">cgdb-<?= LATEST ?>.tar.gz</a> -
-        <a href="http://cgdb.git.sourceforge.net/git/gitweb.cgi?p=cgdb/cgdb;a=blob;f=ChangeLog;hb=HEAD">ChangeLog</a> -
         <a href="http://cgdb.git.sourceforge.net/git/gitweb.cgi?p=cgdb/cgdb;a=blob;f=NEWS;hb=HEAD">NEWS</a>
         </p>
       </div>
@@ -184,47 +58,38 @@
     <a name="git"></a>
     <h4>Git Repository</h4>
 
-      <div class="pagetext">
-
-        <p>
-          The most recent changes to CGDB can be found by cloning
-          a copy of the current repository, 
-          <a href="http://git-scm.com/">Git</a> tree.  You may
-          want to try this to see if a bug is resolved before reporting the
-          error. It should be noted that the git tree is not guaranteed to
-          be stable and is not tested as well as the latest stable release.
-        </p>
-
-        <p>
-          To get a copy of the git repository, use the following command:
-          <code>
-            git clone git://cgdb.git.sourceforge.net/gitroot/cgdb/cgdb
-          </code>
-          After checking out the tree, you will need to generate the configure
-          script before you can configure and build the code.  Use the script
-          "autoregen.sh" at the top level of the source tree to do this
-          automatically.
-        </p>
-
-        <p>
-          <b>Note:</b> Building the code checked out from git will require
-          additional tools installed to generate the autoconf script.  We use
-          the following versions, although others may work for you:
-        </p>
-
-        <ul>
-          <li>aclocal  (GNU automake)   1.9.6</li>
-          <li>autoconf (GNU Autoconf)   2.59</li>
-          <li>m4       (GNU m4)         1.4.4</li>
-        </ul>
-
-        <p>
-          Finally, if you decide to modify the tokenizer library or the config
-          file module, you will need to have flex installed.  We have used flex
-          2.5.4 to build cgdb.
-        </p>
-
-      </div>
+    <div class="pagetext">
+      <p>
+        cgdb's <a href="http://git-scm.com/">git</a> repository is available
+        for development purposes, and may be less stable than official
+        releases of cgdb.
+      </p>
+      <p>
+        Git (anonymous) download command:
+        <code>
+          git clone git://cgdb.git.sourceforge.net/gitroot/cgdb/cgdb
+        </code>
+        After checking out the tree, you will need to generate the configure
+        script before you can configure and build the code.  Run
+        "autogen.sh" at the top level of the source tree to do this
+        automatically.
+      </p>
+      <p>
+        <b>Note:</b> Building the code checked out from git will require
+        additional tools installed to generate the autoconf script.  We use
+        the following versions, although others may work for you:
+      </p>
+      <ul>
+        <li>automake (GNU automake)   1.11.1</li>
+        <li>autoconf (GNU Autoconf)   2.67</li>
+        <li>m4       (GNU m4)         1.4.14</li>
+      </ul>
+      <p>
+        Finally, if you decide to modify the tokenizer library or the config
+        file module, you will need to have flex installed.  We have used flex
+        2.5.35 to build cgdb.
+      </p>
+    </div>
 
 <?
     include "footer.php";
