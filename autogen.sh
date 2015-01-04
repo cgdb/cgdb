05e69b18c94140dfda52ea65af491c799a2c8226
Kevin Zheng <kevinz5000@gmail.com>
2015-01-04 17:09:19 -0600

    Replace autogen with modern equivalents

--- autogen.sh
+++ autogen.sh
@@ -1,24 +1,2 @@
 #!/bin/sh
-
-set -e
-
-rm -rf autom4te.cache/
-
-echo "-- Update configure.in to reflect the new version number"
-if [ "$1" = "" ]; then
-  CGDB_VERSION=`date +%Y%m%d`
-  cp configure.init configure.ac
-  perl -pi -e "s/AC_INIT\(cgdb, (.*)\)/AC_INIT\(cgdb, $CGDB_VERSION\)/g" configure.ac
-fi
-
-echo "-- Running aclocal"
-aclocal -I config
-
-echo "-- Running autoconf"
-autoconf -f
-
-echo "-- Running autoheader"
-autoheader
-
-echo "-- Running automake"
-automake -a
+autoreconf -i
