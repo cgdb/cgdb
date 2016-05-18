de8abd0ea163403706000527a2b96f488f7a9780
Michael Sartain <mikesart@gmail.com>
2016-05-18 16:05:05 -0700

    Allow setting CGDB_VERSION environment for autogen.sh
    Ie, this:
    
      CGDB_VERSION=$(git describe --tags) ./autogen.sh
    
    Would result in a version similar to this:
    
      version v0.6.8-85-gf8c52c6
    
    Signed-off-by: Michael Sartain <mikesart@gmail.com>
    Signed-off-by: Bob Rossi <bob@brasko.net>

--- autogen.sh
+++ autogen.sh
@@ -6,7 +6,7 @@ rm -rf autom4te.cache/
 
 echo "-- Update configure.in to reflect the new version number"
 if [ "$1" = "" ]; then
-  CGDB_VERSION=`date +%Y%m%d`
+  CGDB_VERSION="${CGDB_VERSION:=`date +%Y%m%d`}"
   cp configure.init configure.ac
   perl -pi -e "s/AC_INIT\(cgdb, (.*)\)/AC_INIT\(cgdb, $CGDB_VERSION\)/g" configure.ac
 fi
