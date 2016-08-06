mikesart/cgdb-mikesart

Don't exit on hl_groups_get_attr() failures
Just draw with normal or bold (HLG_LINE_HIGHLIGHT) attributes.

Signed-off-by: Michael Sartain <mikesart@gmail.com>

cgdb/highlight_groups.cpp
@@ -457,7 +457,7 @@ hl_groups_get_attr(hl_groups_ptr hl_groups, enum hl_group_kind kind, int *attr)
     struct hl_group_info *info = lookup_group_info_by_key(hl_groups, kind);
 
     /* Default to normal */
-    *attr = A_NORMAL;
+    *attr = (kind == HLG_LINE_HIGHLIGHT) ? A_BOLD : A_NORMAL;
 
     if (!hl_groups || !info || !attr)
         return -1;

cgdb/scroller.cpp
@@ -317,8 +317,7 @@ void scr_refresh(struct scroller *scr, int focus)
     int highlight_attr;
 
     /* Steal line highlight attribute for our scroll mode status */
-    if (hl_groups_get_attr(hl_groups_instance, HLG_LINE_HIGHLIGHT, &highlight_attr) == -1)
-        highlight_attr = A_BOLD;
+    hl_groups_get_attr(hl_groups_instance, HLG_LINE_HIGHLIGHT, &highlight_attr);
 
     /* Sanity check */
     getmaxyx(scr->win, height, width);

cgdb/cgdb

Signed-off-by: Bob Rossi <bob@brasko.net>

cgdb/highlight_groups.cpp
@@ -473,7 +473,7 @@ hl_groups_get_attr(hl_groups_ptr hl_groups, enum hl_group_kind kind, int *attr)
     struct hl_group_info *info = lookup_group_info_by_key(hl_groups, kind);
 
     /* Default to normal */
-    *attr = A_NORMAL;
+    *attr = (kind == HLG_EXECUTING_LINE_HIGHLIGHT) ? A_BOLD : A_NORMAL;
 
     if (!hl_groups || !info || !attr)
         return -1;

cgdb/scroller.cpp
@@ -308,8 +308,7 @@ void scr_refresh(struct scroller *scr, int focus)
     int highlight_attr;
 
     /* Steal line highlight attribute for our scroll mode status */
-    if (hl_groups_get_attr(hl_groups_instance, HLG_SCROLL_MODE_STATUS, &highlight_attr) == -1)
-        highlight_attr = A_BOLD;
+    hl_groups_get_attr(hl_groups_instance, HLG_SCROLL_MODE_STATUS, &highlight_attr);
 
     /* Sanity check */
     getmaxyx(scr->win, height, width);