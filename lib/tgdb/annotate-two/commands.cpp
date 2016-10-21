a11353431af1b33e009ee750b883f76852ee4df4
Bob Rossi <bob@brasko.net>
2016-10-20 22:25:18 -0400

    Change info sources to use gdbwire instance.
    Signed-off-by: Bob Rossi <bob@brasko.net>

--- lib/tgdb/annotate-two/commands.cpp
+++ lib/tgdb/annotate-two/commands.cpp
@@ -55,15 +55,6 @@ struct commands {
 
     /*@} */
 
-    /* info sources information {{{ */
-    /*@{ */
-
-  /** All of the sources.  */
-    struct ibuf *info_sources_string;
-
-    /*@} */
-    /* }}} */
-
     /* tab completion information {{{ */
     /*@{ */
 
@@ -88,48 +79,6 @@ struct commands {
      */
     struct gdbwire *wire;
 };
-
-static void gdbwire_stream_record_callback(void *context,
-    struct gdbwire_mi_stream_record *stream_record)
-{
-    struct commands *c = (struct commands*)context;
-
-    switch (c->cur_command_state) {
-        case INFO_BREAKPOINTS:
-            /**
-             * When using GDB with annotate=2 and also using interpreter-exec,
-             * GDB spits out the annotations in the MI output. All of these
-             * annotations can be ignored.
-             */
-            break;
-        case COMMAND_COMPLETE:
-            if (stream_record->kind == GDBWIRE_MI_CONSOLE) {
-                char *str = stream_record->cstring;
-                size_t length = strlen(str);
-                if (str[length-1] == '\n') {
-                    str[length-1] = 0;
-                }
-                tgdb_list_append(c->tab_completions, cgdb_strdup(str));
-            }
-            break;
-    }
-
-}
-
-static void gdbwire_async_record_callback(void *context,
-        struct gdbwire_mi_async_record *async_record)
-{
-}
-
-static void send_command_complete_response(struct commands *c)
-{
-    struct tgdb_response *response = (struct tgdb_response *)
-        cgdb_malloc(sizeof (struct tgdb_response));
-    response->header = TGDB_UPDATE_COMPLETIONS;
-    response->choice.update_completions.completion_list =
-        c->tab_completions;
-    tgdb_types_append_command(c->response_list, response);
-}
 
 static void
 commands_send_breakpoints(struct commands *c, struct tgdb_list *bkpt_list)
@@ -191,6 +140,87 @@ static void commands_process_breakpoints(struct commands *c,
     }
 }
 
+static void commands_send_source_files(struct commands *c,
+        struct tgdb_list *source_files)
+{
+    struct tgdb_response *response = (struct tgdb_response *)
+            cgdb_malloc(sizeof (struct tgdb_response));
+
+    response->header = TGDB_UPDATE_SOURCE_FILES;
+    response->choice.update_source_files.source_files = source_files;
+    tgdb_types_append_command(c->response_list, response);
+}
+
+/* This function is capable of parsing the output of 'info source'.
+ * It can get both the absolute and relative path to the source file.
+ */
+static void
+commands_process_info_sources(struct commands *c,
+        struct gdbwire_mi_result_record *result_record)
+{
+    enum gdbwire_result result;
+    struct gdbwire_mi_command *mi_command = 0;
+    result = gdbwire_get_mi_command(GDBWIRE_MI_FILE_LIST_EXEC_SOURCE_FILES,
+        result_record, &mi_command);
+    if (result == GDBWIRE_OK) {
+        struct tgdb_list *source_files = tgdb_list_init();
+        struct gdbwire_mi_source_file *files =
+            mi_command->variant.file_list_exec_source_files.files;
+        while (files) {
+            char *file = (files->fullname)?files->fullname:files->file;
+            tgdb_list_append(source_files, strdup(file));
+            files = files->next;
+        }
+
+        commands_send_source_files(c, source_files);
+
+        gdbwire_mi_command_free(mi_command);
+    }
+}
+
+static void send_command_complete_response(struct commands *c)
+{
+    struct tgdb_response *response = (struct tgdb_response *)
+        cgdb_malloc(sizeof (struct tgdb_response));
+    response->header = TGDB_UPDATE_COMPLETIONS;
+    response->choice.update_completions.completion_list =
+        c->tab_completions;
+    tgdb_types_append_command(c->response_list, response);
+}
+
+static void gdbwire_stream_record_callback(void *context,
+    struct gdbwire_mi_stream_record *stream_record)
+{
+    struct commands *c = (struct commands*)context;
+
+    switch (c->cur_command_state) {
+        case INFO_BREAKPOINTS:
+            /**
+             * When using GDB with annotate=2 and also using interpreter-exec,
+             * GDB spits out the annotations in the MI output. All of these
+             * annotations can be ignored.
+             */
+            break;
+        case COMMAND_COMPLETE:
+            if (stream_record->kind == GDBWIRE_MI_CONSOLE) {
+                char *str = stream_record->cstring;
+                size_t length = strlen(str);
+                if (str[length-1] == '\n') {
+                    str[length-1] = 0;
+                }
+                tgdb_list_append(c->tab_completions, cgdb_strdup(str));
+            }
+            break;
+    }
+
+}
+
+static void gdbwire_async_record_callback(void *context,
+        struct gdbwire_mi_async_record *async_record)
+{
+}
+
+
 static void gdbwire_result_record_callback(void *context,
         struct gdbwire_mi_result_record *result_record)
 {
@@ -200,6 +230,9 @@ static void gdbwire_result_record_callback(void *context,
         switch (c->cur_command_state) {
             case INFO_BREAKPOINTS:
                 commands_process_breakpoints(c, result_record);
+                break;
+            case INFO_SOURCES:
+                commands_process_info_sources(c, result_record);
                 break;
             case COMMAND_COMPLETE:
                 send_command_complete_response(c);
@@ -238,8 +271,6 @@ struct commands *commands_initialize(void)
     c->cur_command_state = VOID_COMMAND;
 
     c->info_source_string = ibuf_init();
-
-    c->info_sources_string = ibuf_init();
 
     c->tab_completions = tgdb_list_init();
 
@@ -290,9 +321,6 @@ void commands_shutdown(struct commands *c)
 
     ibuf_free(c->info_source_string);
     c->info_source_string = NULL;
-
-    ibuf_free(c->info_sources_string);
-    c->info_sources_string = NULL;
 
     tgdb_list_destroy(c->tab_completions);
 
@@ -370,51 +398,6 @@ static void commands_process_info_source(struct commands *c, char a,
     }
 }
 
-static void commands_send_source_files(struct commands *c,
-        struct tgdb_list *list, struct tgdb_list *source_files)
-{
-    struct tgdb_response *response = (struct tgdb_response *)
-            cgdb_malloc(sizeof (struct tgdb_response));
-
-    response->header = TGDB_UPDATE_SOURCE_FILES;
-    response->choice.update_source_files.source_files = source_files;
-    tgdb_types_append_command(list, response);
-}
-
-/* This function is capable of parsing the output of 'info source'.
- * It can get both the absolute and relative path to the source file.
- */
-static void
-commands_process_info_sources(struct commands *c, char a,
-        struct tgdb_list *list)
-{
-    ibuf_addchar(c->info_sources_string, a);
-
-    if (a == '\n') {
-        enum gdbwire_result result;
-        struct gdbwire_mi_command *mi_command = 0;
-        result = gdbwire_interpreter_exec(ibuf_get(c->info_sources_string),
-            GDBWIRE_MI_FILE_LIST_EXEC_SOURCE_FILES, &mi_command);
-        if (result == GDBWIRE_OK) {
-            struct tgdb_list *source_files = tgdb_list_init();
-            struct gdbwire_mi_source_file *files =
-                mi_command->variant.file_list_exec_source_files.files;
-            while (files) {
-                char *file = (files->fullname)?files->fullname:files->file;
-                tgdb_list_append(source_files, strdup(file));
-                files = files->next;
-            }
-
-            commands_send_source_files(c, list, source_files);
-
-            gdbwire_mi_command_free(mi_command);
-
-        }
-
-        ibuf_clear(c->info_sources_string);
-    }
-}
-
 void commands_process(struct commands *c, char a, struct tgdb_list *list)
 {
     // Wow, this is ugly, but I think by the time I'm done with Michael's
@@ -422,7 +405,7 @@ void commands_process(struct commands *c, char a, struct tgdb_list *list)
     c->response_list = list;
 
     if (commands_get_state(c) == INFO_SOURCES) {
-        commands_process_info_sources(c, a, list);
+        gdbwire_push_data(c->wire, &a, 1);
     } else if (commands_get_state(c) == INFO_BREAKPOINTS) {
         gdbwire_push_data(c->wire, &a, 1);
     } else if (commands_get_state(c) == COMMAND_COMPLETE) {
@@ -450,7 +433,6 @@ commands_prepare_for_command(struct annotate_two *a2,
 
     switch (*a_com) {
         case ANNOTATE_INFO_SOURCES:
-            ibuf_clear(c->info_sources_string);
             commands_set_state(c, INFO_SOURCES, NULL);
             break;
         case ANNOTATE_INFO_SOURCE:
