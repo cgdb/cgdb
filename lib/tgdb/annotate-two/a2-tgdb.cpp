mikesart/cgdb-mikesart

Call disassemble w/ address when disassemble can't find frame address.
Signed-off-by: Michael Sartain <mikesart@gmail.com>

lib/tgdb/annotate-two/a2-tgdb.cpp
@@ -309,6 +309,19 @@ int a2_get_current_location(struct annotate_two *a2)
                            ANNOTATE_INFO_FRAME, NULL, 1);
 }
 
+int a2_disassemble(struct annotate_two *a2, int lines)
+{
+    int ret;
+    char *data = NULL;
+
+    data = lines ? sys_aprintf("%d", lines) : NULL;
+    ret = commands_issue_command(a2->c, a2->client_command_list,
+                                 ANNOTATE_DISASSEMBLE, data, 0);
+
+    free(data);
+    return ret;
+}
+
 int a2_disassemble_func(struct annotate_two *a2, int raw, int source,
     const char *file, const char *function)
 {
@@ -387,7 +400,7 @@ const char *a2_return_client_command(struct annotate_two *a2, enum tgdb_command_
         case TGDB_UNTIL:
             ret = "until";
             break;
-        case TGDB_DISASSEMBLE:
+        case TGDB_DISASSEMBLE_INSTR:
             ret = "disassemble";
             break;
         case TGDB_UP:

lib/tgdb/tgdb-base/tgdb_client_interface.cpp
@@ -136,6 +136,11 @@ int tgdb_client_open_new_tty(struct tgdb_client_context *tcc,
         inferior_stdin, inferior_stdout);
 }
 
+int tgdb_client_disassemble(struct tgdb_client_context *tcc, int lines)
+{
+    return a2_disassemble(tcc->tgdb_debugger_context, lines);
+}
+
 int tgdb_client_disassemble_func(struct tgdb_client_context *tcc,
     int raw, int source, const char *file, const char *function)
 {

lib/tgdb/tgdb-base/tgdb_client_interface.h
@@ -376,6 +376,7 @@ pid_t tgdb_client_get_debugger_pid(struct tgdb_client_context *tcc);
 int tgdb_client_open_new_tty(struct tgdb_client_context *tcc,
         int *inferior_stdin, int *inferior_stdout);
 
+int tgdb_client_disassemble(struct tgdb_client_context *tcc, int lines);
 int tgdb_client_disassemble_func(struct tgdb_client_context *tcc,
     int raw, int source, const char *file, const char *function);
 

cgdb/cgdb

Signed-off-by: Bob Rossi <bob@brasko.net>

lib/tgdb/annotate-two/a2-tgdb.cpp
@@ -309,6 +309,19 @@ int a2_get_current_location(struct annotate_two *a2)
                            ANNOTATE_INFO_FRAME, NULL, 1);
 }
 
+int a2_disassemble_pc(struct annotate_two *a2, int lines)
+{
+    int ret;
+    char *data = NULL;
+
+    data = lines ? sys_aprintf("%d", lines) : NULL;
+    ret = commands_issue_command(a2->c, a2->client_command_list,
+                                 ANNOTATE_DISASSEMBLE_PC, data, 0);
+
+    free(data);
+    return ret;
+}
+
 int a2_disassemble_func(struct annotate_two *a2, int raw, int source,
     const char *file, const char *function)
 {

lib/tgdb/tgdb-base/tgdb_client_interface.cpp
@@ -141,6 +141,11 @@ int tgdb_client_open_new_tty(struct tgdb_client_context *tcc,
         inferior_stdin, inferior_stdout);
 }
 
+int tgdb_client_disassemble_pc(struct tgdb_client_context *tcc, int lines)
+{
+    return a2_disassemble_pc(tcc->tgdb_debugger_context, lines);
+}
+
 int tgdb_client_disassemble_func(struct tgdb_client_context *tcc,
     int raw, int source, const char *file, const char *function)
 {

lib/tgdb/tgdb-base/tgdb_client_interface.h
@@ -396,6 +396,7 @@ pid_t tgdb_client_get_debugger_pid(struct tgdb_client_context *tcc);
 int tgdb_client_open_new_tty(struct tgdb_client_context *tcc,
         int *inferior_stdin, int *inferior_stdout);
 
+int tgdb_client_disassemble_pc(struct tgdb_client_context *tcc, int lines);
 int tgdb_client_disassemble_func(struct tgdb_client_context *tcc,
     int raw, int source, const char *file, const char *function);
 