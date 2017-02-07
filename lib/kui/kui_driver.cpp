cgdb/cgdb

commit 7c50f6b680baa8cba3d91fd58d8fda98ba82867c

Remove last direct usage of curses.h/ncurses.h
Signed-off-by: Bob Rossi <bob@brasko.net>

lib/kui/kui_driver.cpp
@@ -47,13 +47,7 @@
 #include <getopt.h>
 #endif
 
-#if HAVE_CURSES_H
-#include <curses.h>
-#elif HAVE_NCURSES_CURSES_H
-#include <ncurses/curses.h>
-#endif /* HAVE_CURSES_H */
-
-//#include "sys_win.h"
+#include "sys_win.h"
 #include "sys_util.h"
 #include "kui.h"
 #include "kui_term.h"
@@ -68,9 +62,8 @@ static void kui_shutdown(int use_endwin)
     kui_ms_destroy(map);
 
     /* Shutdown curses */
-    echo();
     if (use_endwin)
-        endwin();
+        swin_endwin();
     exit(0);
 }
 
@@ -201,7 +194,7 @@ static void parse_long_options(int argc, char **argv)
                 break;
             case '?':
             case 'h':
-                endwin();
+                swin_endwin();
                 usage();
                 kui_shutdown(0);
             default:
@@ -364,13 +357,13 @@ static int create_mappings(struct kui_manager *kuim)
 
 }
 
+/* Original terminal attributes */
+
 int main(int argc, char **argv)
 {
     /* Initialize curses */
-    initscr();
-    noecho();
-    raw();
-    refresh();
+    swin_initscr();
+    swin_refresh();
 
     manager = kui_manager_create(STDIN_FILENO, 40, 1000);
 

commit 7f8223cac0c72e84f8ec85c510309237ba4941ed

Fix KUI test suite
Commit 7c50f6b broke the kui test suite by not putting the kui_driver
into raw mode.

Signed-off-by: Bob Rossi <bob@brasko.net>

lib/kui/kui_driver.cpp
@@ -363,6 +363,7 @@ int main(int argc, char **argv)
 {
     /* Initialize curses */
     swin_initscr();
+    swin_raw();
     swin_refresh();
 
     manager = kui_manager_create(STDIN_FILENO, 40, 1000);

lib/util/sys_win.cpp
@@ -261,3 +261,8 @@ int swin_color_pair(int pair)
 {
     return COLOR_PAIR(pair);
 }
+
+int swin_raw(void)
+{
+    return raw();
+}

lib/util/sys_win.h
@@ -130,4 +130,14 @@ int swin_init_pair(int pair, int f, int b);
 int swin_pair_content(int pair, int *f, int *b);
 int swin_color_pair(int pair); /* COLOR_PAIR(n) */
 
+/**
+ * Put the keyboard into raw mode.
+ *
+ * This is currently only used by the kui_driver.
+ *
+ * @return
+ * 0 on succes or -1 on error
+ */
+int swin_raw(void);
+
 #endif