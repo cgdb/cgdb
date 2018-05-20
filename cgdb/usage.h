c9715a1cb94166243a50471bb3a37ffb8304cdff
Ian Dinwoodie <dinwoodieian@gmail.com>
2018-05-20 18:09:56 -0400

    Updated usage source and header to allow for unit testing
    Signed-off-by: Ian Dinwoodie <dinwoodieian@gmail.com>

--- cgdb/usage.h
+++ cgdb/usage.h
@@ -1,9 +1,11 @@
 #ifndef __USAGE_H__
 #define __USAGE_H__
 
+#include <iostream>
+
 /**
  * This will generate the usage message for CGDB.
  */
-void usage(void);
+void usage(std::ostream& os = std::cout);
 
 #endif /* __USAGE_H__ */
