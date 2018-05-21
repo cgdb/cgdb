9010dc6825e4daaa90b8c507cacda562fcde3fc6
Ian Dinwoodie <dinwoodieian@gmail.com>
2018-05-20 23:33:53 -0400

    Added usage and options message sections for cgdb cli help message test
    Signed-off-by: Ian Dinwoodie <dinwoodieian@gmail.com>

--- catch2/test_usage.cpp
+++ catch2/test_usage.cpp
@@ -4,11 +4,24 @@
 #include <string>
 
 
-TEST_CASE("cgdb help message")
+TEST_CASE("cgdb command line help")
 {
   std::ostringstream oss;
   usage(oss);
 
-  std::string line = "CGDB Usage:";
-  REQUIRE(oss.str().find(line) != std::string::npos);
+  SECTION("cgdb usage message")
+  {
+    std::string usageMsg =
+      "CGDB Usage:\n"
+      "   cgdb [cgdb options] [--] [gdb options]\n"
+      "\n";
+    REQUIRE(oss.str().find(usageMsg) != std::string::npos);
+  }
+
+  SECTION("cgdb options message")
+  {
+    std::string optionsMsg =
+      "CGDB Options:\n";
+    REQUIRE(oss.str().find(optionsMsg) != std::string::npos);
+  }
 }
