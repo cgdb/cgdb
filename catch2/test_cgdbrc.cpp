50a878fae7ada0334fe74bdb25d1d6b71468673f
Ian Dinwoodie <dinwoodieian@gmail.com>
2018-06-03 22:14:53 -0400

    Added test cases for setting cgdb line displays
    Signed-off-by: Ian Dinwoodie <dinwoodieian@gmail.com>

--- catch2/test_cgdbrc.cpp
+++ catch2/test_cgdbrc.cpp
@@ -19,7 +19,7 @@ TEST_CASE("Set arrow style")
   {
     REQUIRE(command_set_arrowstyle("block") == 1);
   }
-  SECTION("Other specified styles defaul to block arrow style")
+  SECTION("All other values yield block arrow style")
   {
     REQUIRE(command_set_arrowstyle("other") == 1);
   }
@@ -39,5 +39,53 @@ TEST_CASE("Set cgdb mode key")
   {
     const char* mode_key = 0;
     REQUIRE(command_set_cgdb_mode_key(mode_key) == -1);
+  }
+}
+
+TEST_CASE("Set executing line display")
+{
+  SECTION("Set short arrow style")
+  {
+    REQUIRE(command_set_executing_line_display("shortarrow") == 0);
+  }
+  SECTION("Set long arrow style")
+  {
+    REQUIRE(command_set_executing_line_display("longarrow") == 0);
+  }
+  SECTION("Set highlight arrow style")
+  {
+    REQUIRE(command_set_executing_line_display("highlight") == 0);
+  }
+  SECTION("Set block arrow style")
+  {
+    REQUIRE(command_set_executing_line_display("block") == 0);
+  }
+  SECTION("Other unspecified arrow style")
+  {
+    REQUIRE(command_set_executing_line_display("other") == 1);
+  }
+}
+
+TEST_CASE("Set selected line display")
+{
+  SECTION("Set short arrow style")
+  {
+    REQUIRE(command_set_selected_line_display("shortarrow") == 0);
+  }
+  SECTION("Set long arrow style")
+  {
+    REQUIRE(command_set_selected_line_display("longarrow") == 0);
+  }
+  SECTION("Set highlight arrow style")
+  {
+    REQUIRE(command_set_selected_line_display("highlight") == 0);
+  }
+  SECTION("Set block arrow style")
+  {
+    REQUIRE(command_set_selected_line_display("block") == 0);
+  }
+  SECTION("Other unspecified arrow style")
+  {
+    REQUIRE(command_set_selected_line_display("other") == 1);
   }
 }
