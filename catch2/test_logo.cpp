54a19082fd19c8fa25e8fdb023d55341c00998d0
Ian Dinwoodie <dinwoodieian@gmail.com>
2018-06-14 09:35:38 -0400

    Added all curses independent tests for the logo source file
    Signed-off-by: Ian Dinwoodie <dinwoodieian@gmail.com>

--- catch2/test_logo.cpp
+++ catch2/test_logo.cpp
@@ -26,6 +26,14 @@ TEST_CASE("Assign a random logo index", "[integration]")
   LogoTestFixture logoTestFixture;
   CHECK(logoTestFixture.getLogoIndex() == 1);
 
-  reset_logo();
+  logo_reset();
   REQUIRE(logoTestFixture.getLogoIndex() != 1);
+}
+
+TEST_CASE("Get the number of available logos", "[integration]")
+{
+  SECTION("ANSI color escape not supported")
+  {
+    REQUIRE(logos_available() == 3);
+  }
 }
