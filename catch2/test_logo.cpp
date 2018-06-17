3d4f251887e8b0f5d7bcf89feaba840588c02b4c
Ian Dinwoodie <dinwoodieian@gmail.com>
2018-06-17 17:12:10 -0400

    Added test case for available logo count with color supported
    Signed-off-by: Ian Dinwoodie <dinwoodieian@gmail.com>

--- catch2/test_logo.cpp
+++ catch2/test_logo.cpp
@@ -1,6 +1,17 @@
+#if HAVE_CONFIG_H
+#include "config.h"
+#endif /* HAVE_CONFIG_H */
+
+#if HAVE_CURSES_H
+#include <curses.h>
+#elif HAVE_NCURSES_CURSES_H
+#include <ncurses/curses.h>
+#endif /* HAVE_CURSES_H */
+
 #include "catch.hpp"
 #include "logo.cpp"
-
+#include "cgdbrc.h"
+#include "highlight_groups.h"
 
 class LogoTestFixture
 {
@@ -10,14 +21,60 @@ class LogoTestFixture
       initializeLogoIndex();
     }
 
-    int getLogoIndex() {
+    ~LogoTestFixture()
+    {
+      if (hl_groups_instance) {
+        free(hl_groups_instance);
+        hl_groups_instance = NULL;
+      }
+    }
+
+    int getLogoIndex()
+    {
       return logoindex;
+    }
+
+    void enableColors()
+    {
+      clog_open(CLOG_CGDB_ID, "%s/cgdb_log%d.txt", "/tmp");
+      initializeCgdbrc();
+      initializeHlGroups();
     }
 
   private:
     void initializeLogoIndex()
     {
       logoindex = 1;
+    }
+
+    void initializeCgdbrc()
+    {
+      cgdbrc_init();
+    }
+
+    void initializeHlGroups()
+    {
+      hl_groups_instance = hl_groups_initialize();
+    }
+};
+
+class CursesTestFixture
+{
+  public:
+    CursesTestFixture()
+    {
+      initscr();
+    }
+
+    ~CursesTestFixture()
+    {
+      endwin();
+    }
+
+    void enableColors()
+    {
+      start_color();
+      use_default_colors();
     }
 };
 
@@ -32,8 +89,18 @@ TEST_CASE("Assign a random logo index", "[integration]")
 
 TEST_CASE("Get the number of available logos", "[integration]")
 {
+  CursesTestFixture cursesTestFixture;
+
   SECTION("ANSI color escape not supported")
   {
     REQUIRE(logos_available() == 3);
+  }
+
+  SECTION("ANSI color escape supported")
+  {
+    cursesTestFixture.enableColors();
+    LogoTestFixture logoTestFixture;
+    logoTestFixture.enableColors();
+    REQUIRE(logos_available() == 7);
   }
 }
