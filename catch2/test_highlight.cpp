ee06f2de999410973bf4364b9952d65c1988172c
Ian Dinwoodie <dinwoodieian@gmail.com>
2018-06-20 23:52:18 -0400

    Updated highlight regex search test case to get complete coverage
    Signed-off-by: Ian Dinwoodie <dinwoodieian@gmail.com>

--- catch2/test_highlight.cpp
+++ catch2/test_highlight.cpp
@@ -1,18 +1,157 @@
-#if HAVE_CONFIG_H
-#include "config.h"
-#endif /* HAVE_CONFIG_H */
-
 #include "catch.hpp"
 #include "highlight.cpp"
+#include <string>
+#include <stdlib.h>
 
 
-TEST_CASE("Highlight regex search match")
+TEST_CASE("Search line for regex match", "[integration]")
 {
-  hl_regex_info* p_hri = NULL;
-  char pattern[4] = "abc";
-  char line[8] = "123abc\n";
-  int icase = 0; // case-insensitive
+  char line[7] = "123abc";
+  std::string regex = "ABC";
   int start, end;
-  int match = hl_regex_search(&p_hri, line, pattern, icase, &start, &end);
-  REQUIRE(match == 1);
+  hl_regex_info info;
+  info.icase = 1; // case insensitive
+  hl_regex_info* info_ptr = &info;
+
+  SECTION("No regex pattern")
+  {
+    regex.clear();
+    REQUIRE(hl_regex_search(&info_ptr, line, regex.c_str(), info.icase,
+                            &start, &end)
+            == -1);
+  }
+
+  SECTION("Recompile the regex")
+  {
+    SECTION("No regex info struct provided")
+    {
+      info_ptr = NULL;
+      REQUIRE(hl_regex_search(&info_ptr, line, regex.c_str(), info.icase,
+                              &start, &end)
+              == 1);
+    }
+
+    SECTION("No regex pattern provided in struct")
+    {
+      info.regex = NULL;
+      REQUIRE(hl_regex_search(&info_ptr, line, regex.c_str(), info.icase,
+                              &start, &end)
+              == 1);
+    }
+
+    SECTION("Case sensitivity in struct does not match provided")
+    {
+      // Note: the provided icase is prioritized over the icase in the struct.
+      REQUIRE(hl_regex_search(&info_ptr, line, regex.c_str(), /*icase=*/ 0,
+                              &start, &end)
+              == 0);
+    }
+
+    SECTION("Regex pattern in struct does not matches provided")
+    {
+      char* alt_regex;
+      alt_regex = (char *)malloc(4*sizeof(char));
+      alt_regex = strdup("ABD");
+      info.regex = alt_regex;
+      REQUIRE(hl_regex_search(&info_ptr, line, regex.c_str(), info.icase,
+                              &start, &end)
+              == 1);
+    }
+  }
+
+  SECTION("Do not recompile the regex")
+  {
+    SECTION("Regex pattern in struct matches provided")
+    {
+      info.regex = (char *)regex.c_str();
+      REQUIRE(hl_regex_search(&info_ptr, line, regex.c_str(), info.icase,
+                              &start, &end)
+              == 1);
+    }
+
+    SECTION("Provide a case insensitivity value of -1")
+    {
+      // Note: the provided icase is prioritized over the icase in the struct.
+      REQUIRE(hl_regex_search(&info_ptr, line, regex.c_str(), /*icase=*/ -1,
+                              &start, &end)
+              == 1);
+    }
+  }
+
+  SECTION("Fail to recompile the regex")
+  {
+    info_ptr = NULL; // force the regex to recompile
+    regex = "["; // provide an invalid regex pattern
+    REQUIRE(hl_regex_search(&info_ptr, line, regex.c_str(), info.icase,
+                            &start, &end)
+            == -1);
+  }
+
+  SECTION("Line ends with newline control character")
+  {
+    info_ptr = NULL;
+    line[6] = '\n';
+    REQUIRE(hl_regex_search(&info_ptr, line, regex.c_str(), info.icase,
+                            &start, &end)
+            == 1);
+  }
+
+  SECTION("Line ends with newline control character")
+  {
+    info_ptr = NULL;
+    line[6] = '\n';
+    REQUIRE(hl_regex_search(&info_ptr, line, regex.c_str(), info.icase,
+                            &start, &end)
+            == 1);
+  }
+}
+
+TEST_CASE("Free highlight regex info struct", "[unit]")
+{
+  SECTION("Null regex info struct")
+  {
+    hl_regex_info* info_ptr = NULL;
+    hl_regex_free(&info_ptr);
+    REQUIRE(info_ptr == NULL);
+  }
+
+  SECTION("Non-null regex info struct")
+  {
+    regex_t regex_map_t;
+    hl_regex_info* info_ptr =
+        (struct hl_regex_info *)malloc(sizeof(struct hl_regex_info));
+    info_ptr->t = regex_map_t;
+    info_ptr->icase = 1;
+    info_ptr->regex = (char *)malloc(4*sizeof(char));
+    info_ptr->regex = strdup("abc");
+    regcomp(&info_ptr->t, "abc", REG_EXTENDED);
+
+    hl_regex_free(&info_ptr);
+    REQUIRE(info_ptr == NULL);
+  }
+}
+
+TEST_CASE("Highlight regex search matches", "[integration]")
+{
+  char line[10] = "test line";
+
+  SECTION("Null regex info struct")
+  {
+    hl_regex_info* info_ptr = NULL;
+    REQUIRE(hl_regex_highlight(&info_ptr, line, HLG_KEYWORD) == NULL);
+  }
+
+  SECTION("Non-null regex info struct")
+  {
+    regex_t regex_map_t;
+    hl_regex_info* info_ptr =
+        (struct hl_regex_info *)malloc(sizeof(struct hl_regex_info));
+    info_ptr->t = regex_map_t;
+    info_ptr->icase = 1;
+    info_ptr->regex = (char *)malloc(4*sizeof(char));
+    info_ptr->regex = strdup("test");
+    regcomp(&info_ptr->t, "test", REG_EXTENDED);
+
+    REQUIRE(hl_regex_highlight(&info_ptr, line, HLG_KEYWORD) != NULL);
+  }
 }
