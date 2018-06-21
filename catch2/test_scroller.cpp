54d9ae04e88c6f95787b23b172857c2aef347bba
Ian Dinwoodie <dinwoodieian@gmail.com>
2018-06-20 23:54:09 -0400

    Updated scroller test cases to include tags
    Signed-off-by: Ian Dinwoodie <dinwoodieian@gmail.com>

--- catch2/test_scroller.cpp
+++ catch2/test_scroller.cpp
@@ -1,7 +1,16 @@
 #include "catch.hpp"
 #include "scroller.cpp"
 
-TEST_CASE("Count the occurances of a character in a string")
+
+TEST_CASE("Count the occurances of a character in a string", "[unit]")
 {
-  REQUIRE(count("aaab", 4, 'a') == 3);
+  SECTION("Character occurs")
+  {
+    REQUIRE(count("aaab", 4, 'a') == 3);
+  }
+
+  SECTION("Character does not occur")
+  {
+    REQUIRE(count("aaab", 4, 'c') == 0);
+  }
 }
