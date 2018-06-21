47541af608b6bc45867c504a2c0bda8a5989e17f
Ian Dinwoodie <dinwoodieian@gmail.com>
2018-06-20 23:55:11 -0400

    Updated sources test case to include tags
    Signed-off-by: Ian Dinwoodie <dinwoodieian@gmail.com>

--- catch2/test_sources.cpp
+++ catch2/test_sources.cpp
@@ -1,13 +1,15 @@
 #include "catch.hpp"
 #include "sources.cpp"
 
-TEST_CASE("Get leading whitespace count")
+
+TEST_CASE("Get leading whitespace count", "[unit]")
 {
   SECTION("No leading whitespace")
   {
     REQUIRE(get_line_leading_ws_count("none", 4) == 0);
   }
-  SECTION("Some leading whitespace")
+
+  SECTION("Leading whitespace")
   {
     REQUIRE(get_line_leading_ws_count("  some", 6) == 2);
   }
