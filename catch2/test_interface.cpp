cb71dd570cc91709a94178ecd09ec619bb9e38f5
Ian Dinwoodie <dinwoodieian@gmail.com>
2018-06-21 22:48:00 -0400

    Added labels to interface source test cases
    Signed-off-by: Ian Dinwoodie <dinwoodieian@gmail.com>

--- catch2/test_interface.cpp
+++ catch2/test_interface.cpp
@@ -1,17 +1,18 @@
 #include "catch.hpp"
 #include "interface.cpp"
 
-TEST_CASE("Get source file window row")
+
+TEST_CASE("Get source file window row", "[unit]")
 {
   REQUIRE(get_src_row() == 0);
 }
 
-TEST_CASE("Get source file window column")
+TEST_CASE("Get source file window column", "[unit]")
 {
   REQUIRE(get_src_col() == 0);
 }
 
-TEST_CASE("Get source file window status height")
+TEST_CASE("Get source file window status height", "[unit]")
 {
   REQUIRE(get_src_status_height() == 1);
 }
