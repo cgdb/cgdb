412ed7dda398ae65a60fb6a3bfe859cb2429b4e8
Ian Dinwoodie <dinwoodieian@gmail.com>
2018-06-21 22:48:00 -0400

    Added labels to command lexer test cases
    Signed-off-by: Ian Dinwoodie <dinwoodieian@gmail.com>

--- catch2/test_command_lexer.cpp
+++ catch2/test_command_lexer.cpp
@@ -1,7 +1,8 @@
 #include "catch.hpp"
 #include "command_lexer.h"
 
-TEST_CASE("Get null token")
+
+TEST_CASE("Get null token", "[integration]")
 {
   const char* token = get_token();
   REQUIRE(!token);
