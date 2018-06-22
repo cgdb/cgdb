9d84e67f0e06f65fee5fd8c04354937a14940621
Ian Dinwoodie <dinwoodieian@gmail.com>
2018-06-21 22:48:00 -0400

    Added test cases for interface source file
    Signed-off-by: Ian Dinwoodie <dinwoodieian@gmail.com>

--- catch2/test_interface.cpp
+++ catch2/test_interface.cpp
@@ -16,3 +16,36 @@ TEST_CASE("Get source file window status height", "[unit]")
 {
   REQUIRE(get_src_status_height() == 1);
 }
+
+TEST_CASE("Get source file window status column", "[integration]")
+{
+  REQUIRE(get_src_status_col() == 0);
+}
+
+TEST_CASE("Get window separation row", "[unit]")
+{
+  REQUIRE(get_sep_row() == 0);
+}
+
+TEST_CASE("Get window separation width", "[unit]")
+{
+  REQUIRE(get_sep_width() == 1);
+}
+
+TEST_CASE("Get gdb window row", "[integration]")
+{
+  SECTION("Vertial window split orientation")
+  {
+    cur_split_orientation = WSO_VERTICAL;
+    REQUIRE(get_gdb_row() == 0);
+  }
+}
+
+TEST_CASE("Get gdb window column", "[integration]")
+{
+  SECTION("Horizonal window split orientation")
+  {
+    cur_split_orientation = WSO_HORIZONTAL;
+    REQUIRE(get_gdb_col() == 0);
+  }
+}
