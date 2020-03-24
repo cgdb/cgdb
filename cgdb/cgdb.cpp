cgdb/cgdb

commit 9480592e90276b2d76492f8eb09357cfd4aefad9

Allow non-standard '~/.cgdb' directory locations.
We now look for an alternate '~/.cgdb' directory by checking the
'CGDB_DIR' environment variable. Addresses issue #212.

Signed-off-by: Yash Lala <yashlala@gmail.com>

cgdb/cgdb.cpp
@@ -804,18 +804,24 @@ static void parse_long_options(int *argc, char ***argv)
  */
 static int init_home_dir(void)
 {
-    /* Get the home directory */
-    char *home_dir = getenv("HOME");
+    /* Check for a nonstandard cgdb dir location */
+    char *cgdb_home_envvar = getenv("CGDB_HOME"); 
 
-    /* Make sure the toplevel .cgdb dir exists */
-    snprintf(cgdb_home_dir, FSUTIL_PATH_MAX, "%s/.cgdb", home_dir);
+    /* Set the cgdb home directory */
+    if (cgdb_home_envvar != NULL) { 
+        snprintf(cgdb_home_dir, FSUTIL_PATH_MAX, cgdb_home_envvar); 
+    } else { 
+        snprintf(cgdb_home_dir, FSUTIL_PATH_MAX, "%s/.cgdb", getenv("HOME"));
+    }
+
+    /* Make sure the toplevel cgdb dir exists */
     if (!fs_util_create_dir(cgdb_home_dir)) {
         printf("Could not create dir %s", cgdb_home_dir);
         return -1;
     }
 
-    /* Try to create full .cgdb/logs directory */
-    snprintf(cgdb_log_dir, FSUTIL_PATH_MAX, "%s/.cgdb/logs", home_dir);
+    /* Try to create log directory */
+    snprintf(cgdb_log_dir, FSUTIL_PATH_MAX, "%s/logs", cgdb_home_dir);
     if (!fs_util_create_dir(cgdb_log_dir)) {
         printf("Could not create dir %s", cgdb_log_dir);
         return -1;

doc/cgdb.texi
@@ -595,11 +595,19 @@ Select the current file.
 @chapter CGDB configuration commands
 @cindex configuring CGDB
 
+By default, CGDB stores its user-specific files (such as command history,
+program logs, and config files) in the @file{~/.cgdb/} directory.  This
+location is configurable; if the environment variable @env{CGDB_DIR} is set
+to a directory name, CGDB will use the specified directory instead of
+@file{~/.cgdb/}. 
+
 There may be several features that you find useful in CGDB.  CGDB is capable
 of automating any of these commands through the use of the config file called
-@file{cgdbrc}.  It looks in @env{$HOME}@file{/.cgdb/} for that file.  If it 
-exists, CGDB executes each line in the file in order.  It is as if the user 
-typed in all the commands into the status bar after the tui was initialized.
+@file{cgdbrc}.  It looks in @env{$CGDB_DIR} for that file, or in
+@env{HOME}@file{/.cgdb/} if the @env{CGDB_DIR} environment variable is not
+set.  If the @file{cgdbrc} file exists, CGDB executes each one of its lines
+in order.  It is as if the user typed in all the commands into the status bar
+after the tui was initialized.
 
 The following variables change the behavior of some aspect of CGDB.  Many
 of these commands may be abbreviated in some way, and all boolean commands

commit 6014f0804cf4f8366067550c9687025a84c836a8

Fix: CGDB_DIR env-var name doesn't match documentation.
CGDB used the 'CGDB_HOME' environment variable to set its data directory
despite the documentation claiming it used the 'CGDB_DIR' variable. The
program's behavior now matches the documentation ('CGDB_DIR' is used).

Fixes bug introduced in 9480592e90276b2d76492f8eb09357cfd4aefad9.

Signed-off-by: Yash Lala <yashlala@gmail.com>

cgdb/cgdb.cpp
@@ -809,7 +809,7 @@ static int init_home_dir(void)
     fs_util_disable_logging();  
 
     /* Check for a nonstandard cgdb dir location */
-    char *cgdb_home_envvar = getenv("CGDB_HOME"); 
+    char *cgdb_home_envvar = getenv("CGDB_DIR"); 
 
     /* Set the cgdb home directory */
     if (cgdb_home_envvar != NULL) { 