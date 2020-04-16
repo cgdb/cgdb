00e26a33579e4b3e553d26fdbe26eb98b4b062ac
Bob Rossi <bob@brasko.net>
2020-04-15 20:14:30 -0400

    CGDB works again with newer versions of GDB
    CGDB annotation support was broken by the following GDB issue,
      https://sourceware.org/bugzilla/show_bug.cgi?id=25190
    Changing CGDB from 'set height 0' to 'set pagination off' works
    around the issue, even though I thought they were identical commands.
    
    Please note, the issue says that it was resolved, but in my
    testing it was not.
    
    Signed-off-by: Bob Rossi <bob@brasko.net>

--- lib/util/fork_util.cpp
+++ lib/util/fork_util.cpp
@@ -188,7 +188,7 @@ int invoke_debugger(const char *path,
     const char *const ANNOTATE_TWO = "--annotate=2";
     const char *const GDBMI = "-i=mi2";
     const char *const SET_ANNOTATE_TWO = "set annotate 2";
-    const char *const SET_HEIGHT_ZERO = "set height 0";
+    const char *const SET_PAGINATION_OFF = "set pagination off";
     char **local_argv;
     int i, j = 0, extra = 8;
     int malloc_size = argc + extra;
@@ -213,7 +213,7 @@ int invoke_debugger(const char *path,
     local_argv[j++] = cgdb_strdup(SET_ANNOTATE_TWO);
 
     local_argv[j++] = cgdb_strdup(EX);
-    local_argv[j++] = cgdb_strdup(SET_HEIGHT_ZERO);
+    local_argv[j++] = cgdb_strdup(SET_PAGINATION_OFF);
 
     /* add the init file that the user did not type */
     if (choice == 0)
