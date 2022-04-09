a42015561bd29101426ca9f530da60664caffbde
Pietro Cerutti <gahr@gahr.ch>
2022-04-09 13:06:45 +0000

    Make kui_tree real C++
    As a side effect, we can get rid of a few checks for nullptr, reduce the
    number of allocations, we provide better encapsulation, and a clearer
    API.
    
    Signed-off-by: Pietro Cerutti <gahr@gahr.ch>

--- lib/kui/kui_tree.cpp
+++ lib/kui/kui_tree.cpp
@@ -1,40 +1,22 @@
 #include <stdlib.h>
 #include <string.h>
 
-#include <memory>
-#include <map>
 #include "kui_tree.h"
 
-/* struct kui_tree_node {{{ */
+kui_tree::node::ptr_type kui_tree::node::create()
+{
+    struct expose_ctor : public node {};
+    return std::make_shared<expose_ctor>();
+}
 
-struct kui_tree_node;
-typedef std::shared_ptr<kui_tree_node> KuiTreeNodeSPtr;
-typedef std::map<int, KuiTreeNodeSPtr> KuiTreeNodeMap;
+kui_tree::kui_tree()
+    : root(node::create())
+{}
 
-/**
- * A node in a kui tree.
- */
-struct kui_tree_node {
-
-    kui_tree_node() : key(0), macro_value(NULL) {}
-
-    /* The keyboard key this node represents. */
-    int key;
-
-    /* If non-null, this node represents the macro value reached.
-     * Otherwise, this node is not the end of the macro.
-     */
-    void *macro_value;
-
-    /**
-     * The children macros from this nodes perspective.
-     */
-    KuiTreeNodeMap children;
-
-    private:
-        kui_tree_node(const kui_tree_node &other);
-        kui_tree_node &operator=(const kui_tree_node &other);
-};
+void kui_tree::insert(int *klist, kui_map *data)
+{
+    insert(root, klist, data);
+}
 
 /**
  * Inserts a map into the tree.
@@ -48,35 +30,32 @@ struct kui_tree_node {
  *
  * @param data
  * The substitution that should take place when the key mapping is reached.
- *
- * @return
- * 0 on success or -1 on error
  */
-static int kui_tree_node_insert(KuiTreeNodeSPtr node, int *klist, void *data)
+void kui_tree::insert(node::ptr_type node, int *klist, kui_map *data)
 {
-    if (!node) {
-        return -1;
-    }
-
     // If at the end of the list, done
     if (klist[0] == 0) {
         node->macro_value = data;
-        return 0;
     } else {
-        KuiTreeNodeSPtr new_node;
-        KuiTreeNodeMap::const_iterator iter = node->children.find(klist[0]);
+        node::ptr_type new_node;
+        auto iter = node->children.find(klist[0]);
 
         if (iter == node->children.end()) {
-            new_node = KuiTreeNodeSPtr(new kui_tree_node());
+            new_node = node::create();
             new_node->key = klist[0];
 
-            node->children.insert(std::make_pair(klist[0], new_node));
+            node->children.emplace(klist[0], new_node);
         } else {
             new_node = iter->second;
         }
 
-        return kui_tree_node_insert(new_node, &klist[1], data);
+        insert(new_node, &klist[1], data);
     }
+}
+
+void kui_tree::erase(int *klist)
+{
+    erase(root, klist);
 }
 
 /**
@@ -89,12 +68,10 @@ static int kui_tree_node_insert(KuiTreeNodeSPtr node, int *klist, void *data)
  * The key sequence to delete.
  * The last item in the list will have a 0 value
  */
-static int kui_tree_node_delete(KuiTreeNodeSPtr node, int *klist)
+void kui_tree::erase(node::ptr_type node, int *klist)
 {
     auto iter = node->children.find(klist[0]);
-    if (iter == node->children.end()) {
-        return -1;
-    } else {
+    if (iter != node->children.end()) {
         // Delete the requested mapping
         if (klist[1] == 0) {
             iter->second->macro_value = NULL;
@@ -111,147 +88,68 @@ static int kui_tree_node_delete(KuiTreeNodeSPtr node, int *klist)
                 node->children.erase(iter);
             }
         } else {
-            kui_tree_node_delete(iter->second, &klist[1]);
+            erase(iter->second, &klist[1]);
             if (iter->second->children.size() == 0 &&
                 iter->second->macro_value == NULL) {
                 node->children.erase(iter);
             }
         }
     }
-
-    return 0;
 }
 
-/* }}} */
-
-/* struct kui_tree {{{ */
-
-/**
- * This data structure is capable of storing a set of maps internally in such a
- * way that it is easy to see what maps are active if a char at a time is fed
- * to this structure.
- *
- * Also, it can determine what mapping was reached if one was found.
- */
-struct kui_tree {
-
-    /* The root of the tree */
-    KuiTreeNodeSPtr root;
-
-    /* The current position pointing into the tree (while looking for a map) */
-    KuiTreeNodeSPtr cur;
-
-    /* The last node found while looking for a map. */
-    /* This happens because maps can be subsets of other maps. */
-    KuiTreeNodeSPtr found_node;
-
-    /* The internal state of the tree ( still looking, map found, not found ) */
-    enum kui_tree_state state;
-    /* If a map was found at all, this is set to 1 while looking, otherwise 0. */
-    int found;
-};
-
-int kui_tree_destroy(struct kui_tree *ktree)
+kui_tree::kui_tree_state kui_tree::get_state() const
 {
-    delete ktree;
-    return 0;
+    return state;
 }
 
-struct kui_tree *kui_tree_create(void)
+void kui_tree::reset_state()
 {
-    struct kui_tree *ktree = new kui_tree();;
-    ktree->root = KuiTreeNodeSPtr(new kui_tree_node());
-
-    return ktree;
+    cur = root;
+    state = kui_tree_state::MATCHING;
+    found = 0;
+    found_node = NULL;
 }
 
-int kui_tree_insert(struct kui_tree *ktree, int *klist, void *data)
+void kui_tree::finalize_state()
 {
-    return kui_tree_node_insert(ktree->root, klist, data);
+    if (found)
+        state = kui_tree_state::FOUND;
 }
 
-int kui_tree_delete(struct kui_tree *ktree, int *klist)
+kui_map *kui_tree::get_data() const
 {
-    kui_tree_node_delete(ktree->root, klist);
-    return 0;
+    return found ? found_node->macro_value : nullptr;
 }
 
-int kui_tree_reset_state(struct kui_tree *ktree)
-{
-    if (!ktree)
-        return -1;
-
-    ktree->cur = ktree->root;
-    ktree->state = KUI_TREE_MATCHING;
-    ktree->found = 0;
-    ktree->found_node = NULL;
-
-    return 0;
-}
-
-int kui_tree_finalize_state(struct kui_tree *ktree)
-{
-    if (!ktree)
-        return -1;
-
-    if (ktree->found)
-        ktree->state = KUI_TREE_FOUND;
-
-    return 0;
-}
-
-int kui_tree_get_state(struct kui_tree *ktree, enum kui_tree_state *state)
-{
-    if (!ktree)
-        return -1;
-
-    *state = ktree->state;
-
-    return 0;
-}
-
-int kui_tree_get_data(struct kui_tree *ktree, void *data)
-{
-    if (!ktree)
-        return -1;
-
-    if (!ktree->found)
-        return -1;
-
-    memcpy(data, &ktree->found_node->macro_value, sizeof (void *));
-
-    return 0;
-}
-
-int kui_tree_push_key(struct kui_tree *ktree, int key, int *map_found)
+bool kui_tree::push_key(int key, int *map_found)
 {
     *map_found = 0;
 
-    if (ktree->state != KUI_TREE_MATCHING)
-        return -1;
+    if (state != kui_tree_state::MATCHING)
+        return false;
 
     /* Check to see if this key matches */
-    auto iter = ktree->cur->children.find(key);
+    auto iter = cur->children.find(key);
 
     /* Not found */
-    if (iter == ktree->cur->children.end()) {
-        ktree->state = KUI_TREE_NOT_FOUND;
-        ktree->cur = NULL;
+    if (iter == cur->children.end()) {
+        state = kui_tree_state::NOT_FOUND;
+        cur = NULL;
     } else {
-        ktree->cur = iter->second;
+        cur = iter->second;
 
         if (iter->second->children.size() == 0) {
-            ktree->state = KUI_TREE_FOUND;
+            state = kui_tree_state::FOUND;
         }
 
         if (iter->second->macro_value) {
-            ktree->found = 1;
-            ktree->found_node = iter->second;
+            found = 1;
+            found_node = iter->second;
             *map_found = 1;
         }
     }
 
-    return 0;
+    return true;
 }
 
 /* }}} */
